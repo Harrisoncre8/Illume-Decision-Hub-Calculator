264d50fd7be78550a994ab61f606881d1a082a1d
Phillip Berg <phillipb1991@msn.com>
2020-01-30 14:32:51 -0600

    set up basic price tool

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Price from '../Price/Price';
+
+function App() {
+  return (
+    <div className="App">
+      <Price/>
+    </div>
+  );
+}
+
+export default App;
