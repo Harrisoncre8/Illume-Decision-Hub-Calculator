0b77cbc4914537013e5cbd9e203b9c44b32ad17d
AwrenNuit <a.rennaker@yahoo.com>
2020-01-30 11:40:58 -0600

    rough draft login layout, updated styling for testing

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import './Login.css';
 
 class Login extends Component{
 
   render(){
     return(
-      <>
-      </>
+      <center>
+        <div className="login-container">
+          <div>
+            <img className="login-logo" src="illume-logo180.png" alt="illume logo" />
+            <span className="login-brand-name">illume decision hub</span>
+          </div>
+          <div className="login-text-field-container">
+            <label className="login-label-email">Email:</label>
+            <input className="text-field login-text-field-email" type="text" placeholder="email" />
+          </div>
+          <div className="login-text-field-container">
+            <label className="login-label-password">Password:</label>
+            <input className="text-field login-text-field-password" type="password" placeholder="password" />
+          </div>
+            <button className="normal-btn login-login-btn">Log In</button>
+          <hr className="login-hr" />
+          <button className="login-register-btn">register</button>
+        </div>
+      </center>
     );
   }
 }
