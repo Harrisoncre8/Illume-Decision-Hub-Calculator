ed407bacfadbce3caa1500e7778c00ebb86c7455
Phillip Berg <phillipb1991@msn.com>
2020-01-31 15:17:18 -0600

    added inital data

--- database.sql
+++ database.sql
@@ -34,7 +34,8 @@ CREATE TABLE "revinue_cost" (
 
 CREATE TABLE "calculators" (
   "id" SERIAL PRIMARY KEY,
-  "calculator" TEXT
+  "calculator" TEXT,
+  "start_id" INT REFERENCES "question_calculator"."id"
 );
 
 CREATE TABLE "inputs" (
@@ -55,6 +56,7 @@ CREATE TABLE "questions" (
 
 CREATE TABLE "split" (
   "id" INT,
+  "calculator_id" INT REFERENCES "calculators"."id",
   "question_id" INT REFERENCES "questions"."id",
   "split_text" TEXT,
   "next" INT
@@ -63,7 +65,7 @@ CREATE TABLE "split" (
 CREATE TABLE "sub_questions" (
   "id" SERIAL PRIMARY KEY,
   "question_id" INT REFERENCES "questions"."id",
-  "order" INT UNIQUE,
+  "order" INT,
   "question" TEXT,
   "response_type" TEXT,
   "help_text" TEXT
@@ -73,11 +75,17 @@ CREATE TABLE "question_calculator" (
   "id" SERIAL PRIMARY KEY,
   "calculator_id" INT REFERENCES "calculators"."id",
   "question_id" INT REFERENCES "questions"."id",
-  "next" int REFERENCES "question_calculator"."id"
+  "next" int
 );
 
 INSERT INTO "questions" ("question", "response_type", "help_text", "sub_questiosn", "split")
-VALUES ()
+VALUES (
+  'Is this for a single sale or total product sales?',
+  'radio',
+  'Single sales are for when you are only considering a single transaction where total product sales considers multiple transactions.',
+  FALSE,
+  TRUE
+),
 (
   'What is your revinue?',
   'number',
@@ -98,4 +106,37 @@ VALUES ()
   'Indirect costs are costs that apply to all sales or services such as gas or rent',
   FALSE,
   FALSE
-)
+),
+(
+  'What is the total number of sales for this product?',
+  'number',
+  'How much of this product or service do you sell?',
+  FALSE,
+  FALSE
+);
+
+INSERT INTO "calculators" ("calculator", "start_id") 
+VALUES ('Define Your Profit Lever', 1),
+('Break Even Pricing', 6),
+('Price Setting', NULL);
+
+INSERT INTO "question_calculator" 
+VALUES (1,1,2),
+(1,2,3),
+(1,3,4),
+(1,4,NULL),
+(1,5,2),
+(2,2,7),
+(2,3,8),
+(2,4,NULL);
+
+INSERT INTO "sub_questions" ("question_id", "order", "question", "response_type", "help_text")
+VALUES (3,1,'What is the rate per of this labor?','number','Consider just one labor rate for this field.'),
+(3,2,'How many hours of labor is done at this rate?','number','Consider just one labor rate for this field.'),
+(3,3,'What are your raw material costs?','number','Raw material costs are things linke lumber or wool.'),
+(3,4,'What are your part costs?','number','Part costs are things like hinges or templates.'),
+(3,5,'What are some other direct costs?','number','Other costs may be things like rental space that is unique to each transaction');
+
+INSERT INTO "split" ("calculator_id", "question_id","split_text","next")
+VALUES(2,1,'Single Product',2),
+(2,1,'Total Product', 5);
