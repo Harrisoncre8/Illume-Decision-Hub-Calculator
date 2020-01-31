c1fb0d27c5b2dccda01c867d21e0c1815033628d
Phillip Berg <phillipb1991@msn.com>
2020-01-31 09:36:47 -0600

    added radio button

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -8,22 +8,48 @@ function Price() {
   const [raw, setRaw] = useState('');
   const [part, setPart] = useState('');
   const [indirect, setIndirect] = useState('');
+  const [type, setType] = useState('single');
 
   useEffect(()=>{
     if(hours && rate && raw && part && indirect ){
       setPrice(hours*rate+raw+part+indirect);
     }
   },[hours, rate, raw, part, indirect, setPrice])
-  
+
   return (
-    <form>
-      hours<input type='number' onChange={(event)=>{setHours(Number(event.target.value))}}/>
-      labor rate<input type='number' onChange={(event)=>{setRate(Number(event.target.value))}}/>
-      Cost of raw materials<input type='number' onChange={(event)=>{setRaw(Number(event.target.value))}}/>
-      Cost of parts<input type='number' onChange={(event)=>{setPart(Number(event.target.value))}}/>
-      Indirect costs<input type='number' onChange={(event)=>{setIndirect(Number(event.target.value))}}/>
-      <p>You're break even price is {price}<br/>You must sell your product at a price higher than {price} to make a profit</p>
-    </form>
+    <div>
+      <form>
+        <input 
+          type="radio" 
+          name="type" 
+          value="single"
+          checked={type === 'single'}
+          onChange={()=>setType('single')}
+        />
+        Single Sale
+        <br/>
+        <input 
+          type="radio" 
+          name="type" 
+          value="total"
+          checked={type === 'total'}
+          onChange={()=>setType('total')}
+        />
+        Total Product
+        <br/>
+        hours<input type='number' onChange={(event)=>{setHours( Number(event.target.value))}}/>
+        labor rate<input type='number' onChange={(event)=>{setRate(Number(event.target.value))}}/>
+        Cost of raw materials<input type='number' onChange={(event)=>{setRaw(Number(event.target.value))}}/>
+        Cost of parts<input type='number' onChange={(event)=>{setPart(Number(event.target.value))}}/>
+        Indirect costs<input type='number' onChange={(event)=>{setIndirect(Number(event.target.value))}}/>
+        {type==='total'? Number of Sales}
+      </form>
+      <p>
+        You're break even price is {price}.
+        <br/>
+        You must sell your product at a price higher than {price} to make a profit.
+      </p>
+    </div>
   );
 }
 
