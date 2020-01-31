aaa1ffc60d978b73759efe60d09f8391fc42e0f0
AwrenNuit <a.rennaker@yahoo.com>
2020-01-31 10:02:18 -0600

    set up form

--- src/components/ProfitLever/ProfitLever.js
+++ src/components/ProfitLever/ProfitLever.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
+import './ProfitLever.css';
 
 function ProfitLever() {
   const [revenue, setRevenue] = useState('');
@@ -7,20 +8,38 @@ function ProfitLever() {
   const [indirectCost, setIndirectCost] = useState('');
 
   return (
-    <form>
+    <div className="profit-lever-container">
       <div>
-        <span>Your Direct Costs</span>
-          <input className="text-field lever-text-field-email" type="text" />
-          <label className="lever-label-email">direct costs</label>
-          <div className="text-field-mask lever-email-mask"></div>
+        <form>
+          <div>
+            <span>What are your Direct Costs?</span>
+              <div className="lever-text-field-container">
+                <input className="text-field lever-text-field-direct-cost" type="text" />
+                <label className="lever-label-direct-cost">direct costs</label>
+                <div className="text-field-mask lever-direct-cost-mask"></div>
+              </div>
+          </div>
+
+          <div>
+            <span>What are your Indirect Costs?</span>
+              <div className="lever-text-field-container">
+                <input className="text-field lever-text-field-indirect-cost" type="text" />
+                <label className="lever-label-indirect-cost">indirect costs</label>
+                <div className="text-field-mask lever-indirect-cost-mask"></div>
+              </div>
+          </div>
+
+          <div>
+            <span>What is your Revenue?</span>
+              <div className="lever-text-field-container">
+                <input className="text-field lever-text-field-revenue" type="text" />
+                <label className="lever-label-revenue">revenue</label>
+                <div className="text-field-mask lever-revenue-mask"></div>
+              </div>
+          </div>
+        </form>
       </div>
-
-        Your Indirect Costs <input />
-
-
-        Your Revenue <input />
-
-    </form>
+    </div>
   );
 }
 
