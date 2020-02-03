087e2b44be216948f97e01d2b5ee7968fb297d8a
AwrenNuit <a.rennaker@yahoo.com>
2020-02-03 12:48:34 -0600

    added basic styling to table

--- src/components/AdminEditUserInfo/AdminEditUserInfo.js
+++ src/components/AdminEditUserInfo/AdminEditUserInfo.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import './AdminEditUserInfo.css';
+import Modal from 'react-awesome-modal';
 
 class AdminEditUserInfo extends Component{
+
+  state = {
+    visible: false
+  }
 
   componentDidMount(){
     this.props.dispatch({type: `GET_ADMIN_USER_INFO`});
   }
 
-  handleClick = (id) => {
+  closeModal = () => {
+    this.setState({visible : false});
+  }
+
+  handleClick = id => {
+    this.setState({visible: true});
   }
 
   render(){
@@ -15,7 +26,7 @@ class AdminEditUserInfo extends Component{
       <center>
         <div className="main-container">
           <h1 className="main-heading">User Information</h1>
-          <table>
+          <table className="admin-user-table">
             <thead>
               <tr>
                 <th>Name</th>
@@ -32,11 +43,19 @@ class AdminEditUserInfo extends Component{
                   <td>{user.company}</td>
                   <td>{user.phone}</td>
                   <td>{user.email}</td>
-                  <td className="admin-edit-user-cell" onClick={()=>this.handleClick(user.id)}></td>
+                  <td className="admin-edit-user-cell" onClick={()=>this.handleClick(user.id)}>Edit Info</td>
                 </tr>
               )}
             </tbody>
           </table>
+          <Modal 
+            visible={this.state.visible}
+            width="400"
+            height="300"
+            effect="fadeInUp"
+            onClickAway={() => this.closeModal()}
+          > 
+          </Modal>
         </div>
       </center>
     );
