5661aa4552389dbf598ffa4d5207c6c961204346
AwrenNuit <a.rennaker@yahoo.com>
2020-02-03 16:05:16 -0600

    created PUT saga for user information updates

--- src/redux/sagas/adminSaga.js
+++ src/redux/sagas/adminSaga.js
@@ -4,14 +4,24 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* getAdminUserInfo(action){
   try{
     const response = yield axios.get(`/api/admin/user-info`);
-    yield put({type: `SET_ADMIN_USER_INFO`, payload: response.data})
+    yield put({type: `SET_ADMIN_USER_INFO`, payload: response.data});
   } catch(error){
     console.log('Error in admin user info GET', error);
   }
 }
 
+function* putAdminUserInfo(action){
+  try{
+    yield axios.get(`/api/admin/user-info`, action.payload);
+    yield put({type: `GET_ADMIN_USER_INFO`});
+  } catch(error){
+    console.log('Error in admin user info PUT', error);
+  }
+}
+
 function* adminSaga() {
   yield takeLatest(`GET_ADMIN_USER_INFO`, getAdminUserInfo);
+  yield takeLatest(`PUT_ADMIN_USER_INFO`, putAdminUserInfo);
 }
   
 export default adminSaga;
