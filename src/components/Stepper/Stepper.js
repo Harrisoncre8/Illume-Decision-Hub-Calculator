b21b628f100cd3bb2611375040848acf34cb0641
Phillip Berg <phillipb1991@msn.com>
2020-02-03 16:18:01 -0600

    now displays radio button options that affect next question

--- src/components/Stepper/Stepper.js
+++ src/components/Stepper/Stepper.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 export default function Stepper() {
   // Using hooks to access redux and saga
   let dispatch = useDispatch();
-  let questionData = useSelector(state => state.question)
+  let questionData = useSelector(state => state.question);
+  let splitData = useSelector(state => state.split);
   let history = useHistory();
 
+	const [splitNext, setSplitNext] = useState('');
 
   function nextPage() {
     if (questionData.next_id == null) {
       const url = questionData.calculator.replace(/ /g, '-').toLowerCase();
       history.push(`/${url}`)
-    } else {
+		} else if(questionData.split){
+			dispatch({ type: 'GET_QUESTION', payload: { querry: { id: splitNext } } })
+		} else {
       dispatch({ type: 'GET_QUESTION', payload: { querry: { id: questionData.next_id } } })
     }
   }
@@ -22,7 +26,25 @@ export default function Stepper() {
     <div className='main-container'>
       {JSON.stringify(questionData)}
       <p>
-        {questionData.question} <input type={questionData.response_type} />{questionData.help_text}</p>
+        {questionData.question} 
+        {questionData.split? 
+          splitData.map((split)=>{
+            return(
+              <>
+									<input 
+										type="radio" 
+										name="next" 
+										value={split.next_id}
+										checked={splitNext === split.next_id}
+										onChange={()=>{setSplitNext(split.next_id)}}
+								/> 
+								{split.split_text}
+							</>
+          )})
+          :
+          <input type={questionData.response_type} />}
+        {questionData.help_text}
+      </p>
       <div className='arrow-left' />
       <div onClick={nextPage} className='arrow-right' />
     </div>
