e51cf12373106975aafa4ac1b99d5eb147ac90da
AwrenNuit <a.rennaker@yahoo.com>
2020-02-03 15:05:30 -0600

    set up industry reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import admin from './adminReducer';
+import industry from './industryReducer';
 import question from './questionReducer';
 
 const rootReducer = combineReducers({
   admin, // stores admin information
+  industry, // stores industry information
   question, // stores questions from the database
 });
 
