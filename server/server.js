3d25d0b998f13c540d513050ced652d744c7cfba
AwrenNuit <a.rennaker@yahoo.com>
2020-02-03 10:32:22 -0600

    set up server route for admin

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // Route includes
-const questionRouter = require('./routes/question.router');
+const adminRouter = require('./routes/admin.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/* Routes */
+// Routes
+app.use(`/api/admin`, adminRouter);
 app.use(`/api/question`, questionRouter);
 
 // Serve static files
@@ -19,7 +20,7 @@ app.use(express.static('build'));
 // App Set //
 const PORT = process.env.PORT || 5000;
 
-/** Listen * */
+// Listen
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
