57231c76aed6bc9b8a14eb2367c2bc87e4fedb97
AwrenNuit <a.rennaker@yahoo.com>
2020-02-03 10:09:23 -0600

    set up basic table structure

--- src/components/AdminEditUserInfo/AdminEditUserInfo.js
+++ src/components/AdminEditUserInfo/AdminEditUserInfo.js
@@ -5,8 +5,33 @@ class AdminEditUserInfo extends Component{
 
   render(){
     return(
-      <>
-      </>
+      <center>
+        <div className="main-container">
+          <h1 className="main-heading">User Information</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Company</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.reduxState.map(user => 
+                <tr key={user.id}>
+                  <td>{user.username}</td>
+                  <td>{user.company}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                  <td></td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </center>
     );
   }
 }
