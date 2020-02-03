65ebc83b9f16d3aa4b6798aca30e85dca40a2a5c
Harrison Nguyen <nguy2173@umn.edu>
2020-02-03 15:49:06 -0600

    Add user page modal

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Modal from 'react-awesome-modal';
 import './Nav.css';
 
 export default function Nav(){
     // react router hook
     let history = useHistory();
+    const [modal, setModal] = useState(false);
+
+    // Change state to open modal
+    const openModal = () => {
+        setModal(true);
+    }
+    // change state to close modal
+    const closeModal = () => {
+        setModal(false);
+    }
 
     return(
         <div className='nav-div'>
@@ -16,6 +27,20 @@ export default function Nav(){
                 Price Setting Calculator</button>
             <button className='circle-btn' onClick={() => history.push('/')}>
                 Log <br/> Out</button>
+            <input type="button" value="Open" onClick={() => openModal()} />
+            <Modal 
+                    visible={modal}
+                    width="400"
+                    height="300"
+                    effect="fadeInUp"
+                    onClickAway={() => closeModal()}
+                >
+                    <div>
+                        <h1>Title</h1>
+                        <p>Some Contents</p>
+                        <a href="javascript:void(0);" onClick={() => closeModal()}>Close</a>
+                    </div>
+                </Modal>
         </div>
     );
 }
