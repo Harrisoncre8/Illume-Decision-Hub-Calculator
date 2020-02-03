4350d6303f70a17aa8b42b988a5c85d795a9b9d4
Phillip Berg <phillipb1991@msn.com>
2020-02-03 15:37:00 -0600

    stepper walks through questions and goes to results page

--- src/components/Stepper/Stepper.js
+++ src/components/Stepper/Stepper.js
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 export default function Stepper(){
     // Using hooks to access redux and saga
     let dispatch = useDispatch();
     let questionData = useSelector(state => state.question)
+    let history = useHistory();
 
-    useEffect(() => {
-        dispatch({type: `GET_QUESTION`});
-        }, [dispatch]);
+
+    function nextPage(){
+        if(questionData.next_id == null){
+            const url = questionData.calculator.replace(/ /g, '-').toLowerCase();
+            history.push(`/${url}`)
+        } else {
+            dispatch({type: 'GET_QUESTION', payload: {querry: {id: questionData.next_id}}})
+        }
+    }
 
     return(
-        <div>
+        <div className='main-container'>
             {JSON.stringify(questionData)}
-            Questions populate here: {questionData}
+            <p>{questionData.question} <input type={questionData.response_type}/>{questionData.help_text}</p>
+            <div className='arrow-left'/>
+            <div onClick={nextPage} className='arrow-right'/>
         </div>
     );
 }
