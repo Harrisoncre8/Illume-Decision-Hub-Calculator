b57169e25ad1939a69bbc0ce83aefdb223e8652f
Phillip Berg <phillipb1991@msn.com>
2020-02-03 16:17:32 -0600

    added call to get radio button options

--- src/redux/sagas/questionSaga.js
+++ src/redux/sagas/questionSaga.js
@@ -2,23 +2,26 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 // worker Saga: will be fired on 'GET_QUESTION' action
-function* getQuestion(action){
-    console.log('In getQuestion with', action)
-    try{
-        const querries = Object.entries(action.payload.querry).reduce((acum, arr)=>{
-            acum += `${arr[0]}=${arr[1]}&`;
-            return acum;
-        }, '').slice(0,-1);
-        const response = yield axios.get(`/api/question?${querries}`);
-        yield put({type: `SET_QUESTION`, payload: response.data[0]})
-    } catch(error){
-        // alert('Sorry, something went wrong while getting questions')
-        console.log('Error getting questions in saga', error);
+function* getQuestion(action) {
+  console.log('In getQuestion with', action)
+  try {
+    const querries = Object.entries(action.payload.querry).reduce((acum, arr) => {
+      acum += `${arr[0]}=${arr[1]}&`;
+      return acum;
+    }, '').slice(0, -1);
+    const response = yield axios.get(`/api/question?${querries}`);
+    if (response.data[0].split) {
+      yield put({type: `GET_SPLIT`, payload: response.data[0].question_id})
     }
+    yield put({ type: `SET_QUESTION`, payload: response.data[0] })
+  } catch (error) {
+    // alert('Sorry, something went wrong while getting questions')
+    console.log('Error getting questions in saga', error);
+  }
 }
 
 function* questionSaga() {
-    yield takeLatest(`GET_QUESTION`, getQuestion);
+  yield takeLatest(`GET_QUESTION`, getQuestion);
 }
-  
+
 export default questionSaga;
