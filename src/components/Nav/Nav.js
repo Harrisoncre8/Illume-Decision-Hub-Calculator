bcd8487ccea1524991ccb3f710fa23ef8783e5f0
Phillip Berg <phillipb1991@msn.com>
2020-02-03 15:36:28 -0600

    profitlever now starts stepper

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './Nav.css';
+import { useDispatch } from 'react-redux';
 
-export default function Nav(){
-    // react router hook
-    let history = useHistory();
+export default function Nav() {
+  // react router hook
+  let history = useHistory();
+  const dispatch = useDispatch();
 
-    return(
-        <div className='nav-div'>
-            <button className='circle-btn' onClick={() => history.push('/break-even')}>
-                Break Even Calculator</button>
-            <button className='circle-btn' onClick={() => history.push('/profit-lever')}>
-                Profit Lever Calculator</button>
-            <button className='circle-btn' onClick={() => history.push('/price-setting')}>
-                Price Setting Calculator</button>
-            <button className='circle-btn' onClick={() => history.push('/')}>
-                Log <br/> Out</button>
-        </div>
-    );
+  function profitLever() {
+    dispatch({type: 'GET_QUESTION', payload: {
+      querry: {
+        start: 1
+      }
+    }})
+    history.push('/questionaire')
+  }
+
+  return (
+    <div className='nav-div'>
+      <button className='circle-btn' onClick={() => history.push('/break-even')}>
+        Break Even Calculator</button>
+      <button className='circle-btn' onClick={() => profitLever()}>
+        Profit Lever Calculator</button>
+      <button className='circle-btn' onClick={() => history.push('/price-setting')}>
+        Price Setting Calculator</button>
+      <button className='circle-btn' onClick={() => history.push('/')}>
+        Log <br /> Out</button>
+    </div>
+  );
 }
-
