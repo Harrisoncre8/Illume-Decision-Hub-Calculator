0770afc9025f6f7b7654546b5902f0ec4e4e77ae
Phillip Berg <phillipb1991@msn.com>
2020-02-03 10:22:51 -0600

    updated initial query text

--- server/routes/question.router.js
+++ server/routes/question.router.js
@@ -4,15 +4,20 @@ const router = express.Router();
 
 // GET route for questions
 router.get('/', (req, res) => {
-    console.log('In getQuestion route with')
-    let sqlQuery = `SELECT * FROM PHIL'S-TABLE-NAME`
-    pool.query(sqlQuery).then(result => {
-        res.send(result.rows);
-    })
-    .catch( error => {
-        console.log('Error with GET question', error);
-        res.sendStatus(500);
-    })
+  console.log('In getQuestion route with')
+  let sqlQuery = `
+    SELECT * FROM question_calculator qc
+    JOIN "questions" q ON "qc"."question_id" = "q"."id"
+    LEFT JOIN "sub_questions" s ON "qc"."id" = "s"."question_id"
+    WHERE "qc"."id" = $1;
+  `
+  pool.query(sqlQuery).then(result => {
+    res.send(result.rows);
+  })
+  .catch(error => {
+    console.log('Error with GET question', error);
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
