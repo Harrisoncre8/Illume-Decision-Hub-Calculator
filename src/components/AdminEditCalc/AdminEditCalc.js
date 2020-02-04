90fa8c40ff14b740f1a7d64438f2cc414b719e54
AwrenNuit <a.rennaker@yahoo.com>
2020-02-04 14:55:48 -0600

    set up boolean toggle for each button

--- src/components/AdminEditCalc/AdminEditCalc.js
+++ src/components/AdminEditCalc/AdminEditCalc.js
@@ -1,18 +1,51 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
 
 class AdminEditCalc extends Component{
 
+  state = {
+    break: false,
+    lever: false,
+    price: false
+  }
+
+  handleBreakClick = () => this.state.break ? this.setState({break:false}) : this.setState({break:true});
+  handleLeverClick = () => this.state.lever ? this.setState({lever:false}) : this.setState({lever:true});
+  handlePriceClick = () => this.state.price ? this.setState({price:false}) : this.setState({price:true});
+
   render(){
     return(
-      <>
-      </>
+      <center>
+        <div className="main-container admin-padding">
+          <div className="admin-button-flex-container">
+            <div className="admin-btn-flex-column">
+              <button 
+                className="circle-btn" 
+                onClick={()=>this.handleBreakClick('break')}
+              >
+                Edit Break Even
+              </button>
+            </div>
+            <div className="admin-btn-flex-column">
+              <button 
+                className="circle-btn" 
+                onClick={()=>this.handlePriceClick('price')}
+              >
+                Edit Price Setting
+              </button>
+            </div>
+            <div className="admin-btn-flex-column">
+              <button 
+                className="circle-btn" 
+                onClick={()=>this.handleLeverClick('lever')}
+              >
+                Edit Profit Lever
+              </button>
+            </div>
+          </div>
+        </div>
+      </center>
     );
   }
 }
 
-const putReduxStateOnProps = (reduxState)=>({
-  calculator: reduxState.OBJECT
-});
-
-export default connect(putReduxStateOnProps)(AdminEditCalc);
+export default AdminEditCalc;
