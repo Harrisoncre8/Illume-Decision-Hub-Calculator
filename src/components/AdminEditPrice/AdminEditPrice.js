dbaa6ee5020f2049bff73e168b130762a64c5a2a
AwrenNuit <a.rennaker@yahoo.com>
2020-02-04 15:28:22 -0600

    set up local state

--- src/components/AdminEditPrice/AdminEditPrice.js
+++ src/components/AdminEditPrice/AdminEditPrice.js
@@ -3,17 +3,45 @@ import {connect} from 'react-redux';
 
 class AdminEditPrice extends Component{
 
+  state = {
+    sales: '',
+    direct: '',
+    indirect: '',
+    pricePer: '',
+    directSub: {
+
+    }
+  }
+
+  componentDidMount(){
+    this.props.dispatch({type: `GET_ADMIN_QUESTION`});
+  }
+
+  componentDidUpdate(prevProps){
+    if(this.props.question !== prevProps.question){
+      // this.setState({
+      //   this.props.question
+      // });
+    }
+  }
+
   render(){
     return(
       <div className="main-container">
+        {JSON.stringify(this.state)}
         <h1 className="main-heading">Edit Price Setting Calculator</h1>
+        {/* {this.props.question.map(q =>
+          <div key={q.id}>
+            <input type="text" value={this.state.sales} onChange={this.handleChange(event, )} />
+          </div>
+        )} */}
       </div>
     );
   }
 }
 
 const putReduxStateOnProps = (reduxState)=>({
-  reduxState: reduxState.OBJECT
+  question: reduxState.adminQuestion
 });
 
 export default connect(putReduxStateOnProps)(AdminEditPrice);
