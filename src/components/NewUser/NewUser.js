92bf4bde03e63160b95a805783ab55f5b4302fbb
Courtney Olesee <court.olson1@gmail.com>
2020-02-05 14:08:02 -0600

    framed for getting user data

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function NewUser(){
+
+  // hooks for redux and sagas
+  let dispatch = useDispatch();
+  let userData = useSelector(state => state.admin.adminUserInfo);
+
+
+  useEffect(() => {
+    dispatch({type: `GET_ADMIN_USER_INFO`});
+    console.log('------------->user info', userData);
+  }, [dispatch]);
+
+
   return(
     <center>
       <div className='main-container'>
-        <h1 className='user-spacing'>Welcome, req.user.name!</h1>
+        <h1 className='user-spacing'>Welcome, {}</h1>
         <h2 className='user-spacing'>What is the Illume Decision Hub Calculator?</h2>
-        <h3 className='user-spacing'>This application is designed to help you generate more profits. 
+        <h3 className='user-spacing'>
+          This application is designed to help you generate more profits. 
           Using conversational language through a “choose your own adventure” format, you will 
           be prompted to enter your financial information to create predictive calculations based 
           on business decisions you can make around product pricing, operating costs, and total revenue. 
