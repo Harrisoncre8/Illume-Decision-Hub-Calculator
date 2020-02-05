4c94b17e8006e7a03940af36653c7f43808094b6
AwrenNuit <a.rennaker@yahoo.com>
2020-02-05 15:36:46 -0600

    troublshooting formatting issues

--- src/components/AdminCalcSubquestion/AdminCalcSubquestion.js
+++ src/components/AdminCalcSubquestion/AdminCalcSubquestion.js
@@ -2,9 +2,23 @@ import React, {useState} from 'react';
 
 export default function AdminCalcSubquestion(props) {
 
+  const [question, setQuestion] = useState(props.question);
+  const [tooltip, setTooltip] = useState(props.tooltip);
+
   return(
     <>
-      <textarea rows="6" cols="30" value={q.question} onChange={(event)=>setQuestion(event.target.value)} />
+      <textarea 
+        rows="6" 
+        cols="30" 
+        value={question} 
+        onChange={(event)=>setQuestion(event.target.value)} 
+      />
+      <textarea 
+        rows="6" 
+        cols="30" 
+        value={tooltip} 
+        onChange={(event)=>setTooltip(event.target.value)}
+      />
     </>
   );
 }
