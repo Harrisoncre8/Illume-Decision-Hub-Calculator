f59600de1ceaa77b9da7d7eb467c8e63330f4841
AwrenNuit <a.rennaker@yahoo.com>
2020-02-05 13:11:24 -0600

    updated reducer to clear state if question has no subquestions

--- src/components/AdminCalcQuestion/AdminCalcQuestion.js
+++ src/components/AdminCalcQuestion/AdminCalcQuestion.js
@@ -1,21 +1,29 @@
 import React, {useState, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function AdminCalcQuestion(props) {
 
+  const subQuestion = useSelector((state)=>state.admin.adminSubquestion);
+
+
   const dispatch = useDispatch();
-  const [id, setID] = useState(props.id);
   const [question, setQuestion] = useState(props.question);
   const [tooltip, setTooltip] = useState(props.tooltip);
 
   useEffect(()=>{
-    if(id === 3 || id === 4){
-      dispatch({type: `GET_ADMIN_SUB_QUESTION`, payload: id});
+    if(props.id === 3 || props.id === 4){
+      dispatch({type: `GET_ADMIN_SUB_QUESTION`, payload: props.id});
     }
-  }, [id]);
+    else if(props.id !== 3 || props.id !== 4){
+      dispatch({type: `CLEAR_ADMIN_SUB_QUESTION`});
+    }
+  }, [props.id]);
 
   return(
     <>
+      {subQuestion.map(q=>
+        <textarea rows="6" cols="30" value={q.question} onChange={(event)=>setQuestion(event.target.value)} />
+      )}
       <textarea rows="6" cols="30" value={question} onChange={(event)=>setQuestion(event.target.value)} />
       <textarea rows="6" cols="30" value={tooltip} onChange={(event)=>setTooltip(event.target.value)} />
     </>
--- src/redux/reducers/adminReducer.js
+++ src/redux/reducers/adminReducer.js
@@ -4,7 +4,15 @@ import { combineReducers } from 'redux';
 const adminQuestion = (state = [], action) => action.type === `SET_ADMIN_QUESTION` ? action.payload : state;
 
 // Store calculator sub-questions for admin to edit
-const adminSubquestion = (state = [], action) => action.type === `SET_ADMIN_SUB_QUESTION` ? action.payload : state;
+const adminSubquestion = (state = [], action) => {
+  if(action.type === `SET_ADMIN_SUB_QUESTION`){
+    return action.payload;
+  }
+  else if(action.type === `CLEAR_ADMIN_SUB_QUESTION`){
+    return '';
+  }
+  return state;
+}
 
 // Store user information for admin to edit
 const adminUserInfo = (state = [], action) => action.type === `SET_ADMIN_USER_INFO` ? action.payload : state;
