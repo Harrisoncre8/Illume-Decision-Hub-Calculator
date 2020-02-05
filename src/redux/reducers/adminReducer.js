b28033558a970eef77f43f43a3fe646c32e5aacf
AwrenNuit <a.rennaker@yahoo.com>
2020-02-05 10:44:35 -0600

    set up reducer to store subquestions

--- src/redux/reducers/adminReducer.js
+++ src/redux/reducers/adminReducer.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
 
-// Store user information for admin to edit
+// Store calculator questions for admin to edit
 const adminQuestion = (state = [], action) => action.type === `SET_ADMIN_QUESTION` ? action.payload : state;
+
+// Store calculator sub-questions for admin to edit
+const adminSubquestion = (state = [], action) => action.type === `SET_ADMIN_SUB_QUESTION` ? action.payload : state;
 
 // Store user information for admin to edit
 const adminUserInfo = (state = [], action) => action.type === `SET_ADMIN_USER_INFO` ? action.payload : state;
   
 export default combineReducers({
   adminQuestion,
+  adminSubquestion,
   adminUserInfo,
 });
