534b63e2c316a90e9de5d70e45622203ef82cb88
AwrenNuit <a.rennaker@yahoo.com>
2020-02-06 09:45:54 -0600

    changed component from class to function, created switch statement to make sure only one calculator is open at a time

--- src/components/AdminEditCalc/AdminEditCalc.js
+++ src/components/AdminEditCalc/AdminEditCalc.js
@@ -1,76 +1,90 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './AdminEditCalc.css';
 import AdminEditPrice from '../AdminEditPrice/AdminEditPrice';
 import AdminEditBreak from '../AdminEditBreak/AdminEditBreak';
 import AdminEditLever from '../AdminEditLever/AdminEditLever';
 
-class AdminEditCalc extends Component{
+export default function AdminEditCalc() {
 
-  state = {
-    break: false,
-    lever: false,
-    price: false
+  const [breakEven, setBreakEven] = useState(false);
+  const [lever, setLever] = useState(false);
+  const [price, setPrice] = useState(false);
+
+  const handleBreakClick = () => breakEven ? setBreakEven(false) : closeOthers('break');
+  const handleLeverClick = () => lever ? setLever(false) : closeOthers('lever');
+  const handlePriceClick = () => price ? setPrice(false) : closeOthers('price');
+
+  const closeOthers = (state) => {
+    switch (state){
+      case 'break':
+        setBreakEven(true); 
+        setLever(false); 
+        setPrice(false);
+        break;
+      case 'lever':
+        setLever(true);
+        setBreakEven(false);
+        setPrice(false);
+        break;
+      case 'price':
+        setPrice(true);
+        setBreakEven(false);
+        setLever(false);
+        break;
+    }
   }
 
-  handleBreakClick = () => this.state.break ? this.setState({break:false}) : this.setState({break:true});
-  handleLeverClick = () => this.state.lever ? this.setState({lever:false}) : this.setState({lever:true});
-  handlePriceClick = () => this.state.price ? this.setState({price:false}) : this.setState({price:true});
-
-  render(){
-    return(
-      <center>
-        <div className="main-container admin-padding">
-          <div className="admin-button-flex-container">
-            <div className="admin-btn-flex-column">
-              <button 
-                className="circle-btn" 
-                onClick={()=>this.handleBreakClick('break')}
-              >
-                Edit Break Even
-              </button>
-            </div>
-            <div className="admin-btn-flex-column">
-              <button 
-                className="circle-btn" 
-                onClick={()=>this.handlePriceClick('price')}
-              >
-                Edit Price Setting
-              </button>
-            </div>
-            <div className="admin-btn-flex-column">
-              <button 
-                className="circle-btn" 
-                onClick={()=>this.handleLeverClick('lever')}
-              >
-                Edit Profit Lever
-              </button>
-            </div>
+  return(
+    <center>
+      <div className="main-container admin-padding">
+        <div className="admin-button-flex-container">
+          <div className="admin-btn-flex-column">
+            <button 
+              className="circle-btn" 
+              onClick={handleBreakClick}
+            >
+              Edit Break Even
+            </button>
+          </div>
+          <div className="admin-btn-flex-column">
+            <button 
+              className="circle-btn" 
+              onClick={handlePriceClick}
+            >
+              Edit Price Setting
+            </button>
+          </div>
+          <div className="admin-btn-flex-column">
+            <button 
+              className="circle-btn" 
+              onClick={handleLeverClick}
+            >
+              Edit Profit Lever
+            </button>
           </div>
         </div>
-          {this.state.price ?
-            <div className="admin-edit-calc-render-container">
-              <AdminEditPrice />
-            </div>
-            :
-            ''
-          }
-          {this.state.break ?
-            <div className="admin-edit-calc-render-container">
-              <AdminEditBreak />
-            </div>
-            :
-            ''
-          }
-          {this.state.lever ?
-            <div className="admin-edit-calc-render-container">
-              <AdminEditLever />
-            </div>
-            :
-            ''
-          }
-      </center>
-    );
-  }
+      </div>
+        {price ?
+          <div className="admin-edit-calc-render-container">
+            <AdminEditPrice />
+          </div>
+          :
+          ''
+        }
+        {breakEven ?
+          <div className="admin-edit-calc-render-container">
+            <AdminEditBreak />
+          </div>
+          :
+          ''
+        }
+        {lever ?
+          <div className="admin-edit-calc-render-container">
+            <AdminEditLever />
+          </div>
+          :
+          ''
+        }
+    </center>
+  );
 }
-
-export default AdminEditCalc;
