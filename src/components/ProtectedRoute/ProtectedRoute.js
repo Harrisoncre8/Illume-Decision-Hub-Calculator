f751d72882f6aa6994f6bcf98d82a637cae45565
Phillip Berg <phillipb1991@msn.com>
2020-02-06 10:06:10 -0600

    route protected if leftover user is an empty object

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -27,7 +27,7 @@ const ProtectedRoute = (props) => {
 
   let ComponentToShow;
 
-  if(user) {
+  if(user && user.id) {
     // if the user is logged in (only logged in users have ids)
     // show the component that is protected
     ComponentToShow = ComponentToProtect;
