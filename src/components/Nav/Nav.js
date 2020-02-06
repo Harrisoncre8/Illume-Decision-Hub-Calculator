2510d0b51cd137dd1ea1351b1121e1637bd546ba
AwrenNuit <a.rennaker@yahoo.com>
2020-02-06 17:04:24 -0600

    added margin to buttons, button color changes based on which page the user is on

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './Nav.css';
-import { useDispatch } from 'react-redux';
 
 export default function Nav() {
 
-  // react router hook
-  let history = useHistory();
+  // Set react router hook
+  const history = useHistory();
   const dispatch = useDispatch();
+  const questionData = useSelector(state=>state.question.calculator_id);
+  const [breakEven, setBreakEven] = useState('');
+  const [lever, setLever] = useState('');
+  const [price, setPrice] = useState('');
+  const [profile, setProfile] = useState('');
 
+  // Run on component mount
+  useEffect(()=>{
+    if(history.location.pathname === '/user'){
+      setProfile('circle-btn-active');
+    }
+    else if(questionData === 1){
+      setLever('circle-btn-active');
+      setBreakEven('');
+      setPrice('');
+    }
+    else if(questionData === 2){
+      setBreakEven('circle-btn-active');
+      setLever('');
+      setPrice('');
+    }
+    else if(questionData === 3){
+      setPrice('circle-btn-active');
+      setBreakEven('');
+      setLever('');
+    }
+  }, [questionData]);
+
+  // Log user out, push history to login page
+  const logout = () => {
+    dispatch({type: 'LOGOUT'});
+    history.push('/');
+  }
+
+  // Push history to user profile
+  const pushHistoryToProfile = () => history.push('/user');
+
+  // Set stepper starting point
   const setStart = id => {
     dispatch({ type: 'CLEAR_PREVIOUS_QUESTION' });
     dispatch({type: 'GET_QUESTION', payload: {query: {start: id}}});
     history.push('/questionnaire');
   }
 
-  function logout() {
-    dispatch({type: 'LOGOUT'});
-    history.push('/');
-  }
-
   return (
-    <div className='nav-div'>
-      <button className='circle-btn' onClick={()=>setStart(2)}>
-        Break Even Calculator</button>
-      <button className='circle-btn' onClick={()=>setStart(1)}>
-        Profit Lever Calculator</button>
-      <button className='circle-btn' onClick={()=>setStart(3)}>
-        Price Setting Calculator</button>
-      <button className='circle-btn' onClick={logout}>
-        Log <br /> Out</button>
-    </div>
+    <center>
+      {/* <ul className="nav-ul">
+        <li className="nav-li">Break Even</li>
+        <li className="nav-li">Price Setting</li>
+        <li className="nav-li">Profit Lever</li>
+        <li className="nav-li">Profile</li>
+        <li className="nav-li">Log Out</li>
+      </ul> */}
+
+
+
+      <button className={`circle-btn nav-btn ${breakEven}`} onClick={()=>setStart(2)}>
+        Break Even Calculator
+      </button>
+      <button className={`circle-btn nav-btn ${lever}`} onClick={()=>setStart(1)}>
+        Profit Lever Calculator
+      </button>
+      <button className={`circle-btn nav-btn ${price}`} onClick={()=>setStart(3)}>
+        Price Setting Calculator
+      </button>
+      <button className={`circle-btn nav-btn ${profile}`} onClick={pushHistoryToProfile}>
+        Profile <br /> Page
+      </button>
+      <button className='circle-btn nav-btn' onClick={logout}>
+        Log <br /> Out
+      </button>
+    </center>
   );
 }
