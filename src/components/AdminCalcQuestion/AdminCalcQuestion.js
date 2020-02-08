9d6d5a4696cfae27d1cd4bd5210569fd7dcf170a
AwrenNuit <a.rennaker@yahoo.com>
2020-02-08 13:15:01 -0600

    turned each field into a form, calculator ID properly gets passed to child components now

--- src/components/AdminCalcQuestion/AdminCalcQuestion.js
+++ src/components/AdminCalcQuestion/AdminCalcQuestion.js
@@ -10,18 +10,19 @@ export default function AdminCalcQuestion(props) {
   const [question, setQuestion] = useState(props.question);
   const [tooltip, setTooltip] = useState(props.tooltip);
 
-  // Dispatch to saga to update question and tooltip in database
-  const handleSave = () => {
-    let id = [props.id, question, tooltip, props.calcID];
-    dispatch({type: `PUT_ADMIN_QUESTION`, payload: id});
-  }
-
   // Runs when component mounts
   useEffect(()=>{
     if(props.id === 3 || props.id === 4){
       dispatch({type: `GET_ADMIN_SUB_QUESTION`, payload: props.id});
     }
   }, [props.id]);
+
+  // Dispatch to saga to update question and tooltip in database
+  const handleSubmit = e => {
+    e.preventDefault();
+    let id = [props.id, question, tooltip, props.calcID];
+    dispatch({type: `PUT_ADMIN_QUESTION`, payload: id});
+  }
 
   // Map through sub-questions, increment question number for each question
   const subMap = () => {
@@ -41,26 +42,28 @@ export default function AdminCalcQuestion(props) {
 
   return(
     <>
-      <div className="admin-calc-question-label">Question:</div>
-      <textarea 
-        className="admin-calc-question-textfield"
-        rows="6" 
-        cols="30" 
-        value={question} 
-        onChange={(event)=>setQuestion(event.target.value)} 
-        placeholder="enter question"
-      />
-      <div  className="admin-calc-tooltip-label">Tooltip:</div>
-      <textarea 
-        rows="6" 
-        cols="30" 
-        value={tooltip} 
-        onChange={(event)=>setTooltip(event.target.value)} 
-        placeholder="enter tooltip"
-      />
-      <div>
-        <button className="normal-btn admin-edit-calc-btn" onClick={handleSave}>SAVE</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="admin-calc-question-label">Question:</div>
+        <textarea 
+          className="admin-calc-question-textfield"
+          rows="6" 
+          cols="30" 
+          value={question} 
+          onChange={(event)=>setQuestion(event.target.value)} 
+          placeholder="enter question"
+        />
+        <div  className="admin-calc-tooltip-label">Tooltip:</div>
+        <textarea 
+          rows="6" 
+          cols="30" 
+          value={tooltip} 
+          onChange={(event)=>setTooltip(event.target.value)} 
+          placeholder="enter tooltip"
+        />
+        <div>
+          <button className="normal-btn admin-edit-calc-btn" type="submit">SAVE</button>
+        </div>
+      </form>
       <hr />
       <div>
         {subMap()}
--- src/components/AdminCalcSubquestion/AdminCalcSubquestion.js
+++ src/components/AdminCalcSubquestion/AdminCalcSubquestion.js
@@ -10,33 +10,36 @@ export default function AdminCalcSubquestion(props) {
   // const [show, setShow]
 
   // Dispatch to saga to update question and tooltip in database
-  const handleSave = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
     let id = [props.id, question, tooltip, props.calcID];
     dispatch({type: `PUT_ADMIN_SUB_QUESTION`, payload: id});
   }
 
   return(
     <>
-      <div className="admin-calc-question-label">Sub-question:</div>
-      <textarea 
-        className="admin-calc-question-textfield"
-        rows="6" 
-        cols="30" 
-        value={question} 
-        onChange={(event)=>setQuestion(event.target.value)} 
-        placeholder="enter sub-question"
-      />
-      <div className="admin-calc-tooltip-label">Tooltip:</div>
-      <textarea 
-        rows="6" 
-        cols="30" 
-        value={tooltip} 
-        onChange={(event)=>setTooltip(event.target.value)}
-        placeholder="enter tooltip"
-      />
-      <div>
-        <button className="normal-btn admin-edit-calc-btn" onClick={handleSave}>SAVE</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="admin-calc-question-label">Sub-question:</div>
+        <textarea 
+          className="admin-calc-question-textfield"
+          rows="6" 
+          cols="30" 
+          value={question} 
+          onChange={(event)=>setQuestion(event.target.value)} 
+          placeholder="enter sub-question"
+        />
+        <div className="admin-calc-tooltip-label">Tooltip:</div>
+        <textarea 
+          rows="6" 
+          cols="30" 
+          value={tooltip} 
+          onChange={(event)=>setTooltip(event.target.value)}
+          placeholder="enter tooltip"
+        />
+        <div>
+          <button className="normal-btn admin-edit-calc-btn" type="submit">SAVE</button>
+        </div>
+      </form>
       <hr />
     </>
   );
