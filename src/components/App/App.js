1e862d02900eee9ba8e1a7243170e5e236ca8eb2
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 15:03:05 -0600

    added footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useCallback } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import './App.css';
 import Admin from '../Admin/Admin';
@@ -7,6 +8,7 @@ import AdminEditCalc from '../AdminEditCalc/AdminEditCalc';
 import AdminEditIndustry from '../AdminEditIndustry/AdminEditIndustry';
 import AdminEditUserInfo from '../AdminEditUserInfo/AdminEditUserInfo';
 import BreakEven from '../BreakEven/BreakEven';
+import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import NewUser from '../NewUser/NewUser';
 import PriceSetting from '../PriceSetting/PriceSetting';
@@ -14,7 +16,6 @@ import ProfitLever from '../ProfitLever/ProfitLever';
 import Register from '../Register/Register';
 import Stepper from '../Stepper/Stepper';
 import User from '../User/User';
-import { useDispatch } from 'react-redux';
 
 export default function App() {
   // gets user info on all pages DO NOT REMOVE
@@ -25,20 +26,19 @@ export default function App() {
   
   return (
     <Router>
-      <div className="App">
-        <Route exact path='/' component={Login} />
-        <ProtectedRoute exact path='/admin' component={Admin} />
-        <ProtectedRoute exact path='/admin/edit-calculator-info' component={AdminEditCalc} />
-        <ProtectedRoute exact path='/admin/edit-industry-info' component={AdminEditIndustry} />
-        <ProtectedRoute exact path='/admin/edit-user-info' component={AdminEditUserInfo} />
-        <ProtectedRoute exact path='/break-even-pricing' component={BreakEven} /> 
-        <ProtectedRoute exact path='/price-setting' component={PriceSetting} />
-        <ProtectedRoute exact path='/define-your-profit-lever' component={ProfitLever} />
-        <ProtectedRoute exact path='/questionnaire' component={Stepper} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/new-user' component={NewUser} />
-        <Route exact path ='/user' component={User} />
-      </div>
+      <Route exact path='/' component={Login} />
+      <ProtectedRoute exact path='/admin' component={Admin} />
+      <ProtectedRoute exact path='/admin/edit-calculator-info' component={AdminEditCalc} />
+      <ProtectedRoute exact path='/admin/edit-industry-info' component={AdminEditIndustry} />
+      <ProtectedRoute exact path='/admin/edit-user-info' component={AdminEditUserInfo} />
+      <ProtectedRoute exact path='/break-even-pricing' component={BreakEven} />
+      <ProtectedRoute exact path='/price-setting' component={PriceSetting} />
+      <ProtectedRoute exact path='/define-your-profit-lever' component={ProfitLever} />
+      <ProtectedRoute exact path='/questionnaire' component={Stepper} />
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/new-user' component={NewUser} />
+      <Route exact path ='/user' component={User} />
+      <Footer />
     </Router>
   );
 }
