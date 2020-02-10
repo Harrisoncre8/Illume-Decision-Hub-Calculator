88ba3a4c750d3d7bc3b360abdc042ac088e01238
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 10:11:29 -0600

    added admin industry GET saga

--- src/redux/sagas/industrySaga.js
+++ src/redux/sagas/industrySaga.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+function* getAdminIndustry(action){
+  try{
+    const response = yield axios.get(`/api/admin/industry`);
+    yield put({type: `SET_INDUSTRY`, payload: response.data})
+  } catch(error){
+    console.log('Error in admin industry GET', error);
+  }
+}
+
 function* getIndustry(action){
   try{
     const response = yield axios.get(`/api/industry`);
     yield put({type: `SET_INDUSTRY`, payload: response.data})
   } catch(error){
-    console.log('Error in admin user info GET', error);
+    console.log('Error in industry GET', error);
   }
 }
 
 function* industrySaga() {
+  yield takeLatest(`GET_ADMIN_INDUSTRY`, getAdminIndustry);
   yield takeLatest(`GET_INDUSTRY`, getIndustry);
 }
   
