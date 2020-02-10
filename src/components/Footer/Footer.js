a31d02a6f49e5156fb1c98bacf72d619ceb638d1
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 15:50:38 -0600

    added history push to homepage when logo is clicked

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Footer.css';
 
 export default function Footer() {
 
   const date = new Date().getFullYear();
+  const history = useHistory();
+
+  // Return to homepage when logo is clicked
+  const pushToHome = () => history.push(`/`)
+
   return(
     <>
       <div className="whitespace"></div>
       <footer className="footer-main-container">
-        <img className="footer-logo" src="illume-logo180.png" alt="illume logo" />
+        <img className="footer-logo" src="illume-logo180.png" alt="illume logo" onClick={pushToHome} />
         <span className="footer-brand-name">illume decision hub</span>
         <div className="footer-span-container">
           <span className="footer-a-span">
