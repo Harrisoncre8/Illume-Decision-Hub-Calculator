9fc7745ff482d707a13350f130c5acfd6d71d227
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 15:46:35 -0600

    added dynamically-rendered copyright year, changed a tag text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,19 @@ import './Footer.css';
 
 export default function Footer() {
 
+  const date = new Date().getFullYear();
   return(
     <>
       <div className="whitespace"></div>
       <footer className="footer-main-container">
         <img className="footer-logo" src="illume-logo180.png" alt="illume logo" />
         <span className="footer-brand-name">illume decision hub</span>
-        <span className="footer-a-span">
-          View our main site &nbsp;
-          <a className="footer-a" href="https://illume-pricing.com/">HERE</a>
-        </span>
+        <div className="footer-span-container">
+          <span className="footer-a-span">
+            &copy; {date} &nbsp;
+            <a className="footer-a" href="https://illume-pricing.com/">Illume Pricing</a>
+          </span>
+        </div>
       </footer>
     </>
   );
