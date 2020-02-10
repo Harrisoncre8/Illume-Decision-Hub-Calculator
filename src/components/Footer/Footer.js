240c52e3d7613d6e270b9972008fff1c4c4bee85
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 15:16:28 -0600

    created stylesheet, added background color to footer,brought in logo and brand name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Footer.css';
 
-export default function Footer{
+export default function Footer() {
 
-    return(
-      <>
-      </>
-    );
+  return(
+    <>
+      <div className="whitespace"></div>
+      <footer className="footer-main-container">
+        <img className="footer-logo" src="illume-logo180.png" alt="illume logo" />
+        <span className="footer-brand-name">illume decision hub</span>
+        <span>
+          <a href="https://illume-pricing.com/" />
+        </span>
+      </footer>
+    </>
+  );
 }
