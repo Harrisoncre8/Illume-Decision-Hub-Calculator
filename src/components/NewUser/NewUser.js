a959af05efd4d18a6d5679381842b0e53193ab23
AwrenNuit <a.rennaker@yahoo.com>
2020-02-11 10:16:57 -0600

    created stylesheet, aligned text left, added margin to bottom of text container

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import './NewUser.css';
 import Nav from '../Nav/Nav';
 import QuestionCheckboxes from '../QuestionCheckboxes/QuestionCheckboxes';
 
@@ -9,6 +10,7 @@ export default function NewUser(){
   const user = useSelector(state => state.userInfo);
   const userID = useSelector(state => state.user.id);
 
+  // Run on component mount
   useEffect(()=>{
     dispatch({type: `GET_USER_INFO`, payload: userID});
   }, [userID]);
@@ -19,17 +21,19 @@ export default function NewUser(){
       <div className='main-container'>
         <h1 className='user-spacing'>Welcome, {user[0] && user[0].name}!</h1>
         <h3 className='user-spacing'>What is the Illume Decision Hub Calculator?</h3>
-        <h3 className='user-spacing'>
-          This application is designed to help you generate more profits. 
-          Using conversational language through a “choose your own adventure” format, you will 
-          be prompted to enter your financial information to create predictive calculations based 
-          on business decisions you can make around product pricing, operating costs, and total revenue. 
-          This will help you better understand how changes in your decision-making and pricing could affect 
-          your future profitability.
-          <br/>
-          <br/>
-          Click on a Calculator above to begin!
-        </h3>
+        <div className="new-user-text-container">
+          <p className="results-text">This application is designed to help you generate more profits.</p>
+          <p className="results-text">
+            Using conversational language through a “choose your own adventure” format, you will 
+            be prompted to enter your financial information to create predictive calculations based 
+            on business decisions you can make around product pricing, operating costs, and total revenue.
+          </p>
+          <p className="results-text">
+            This will help you better understand how changes in your decision-making and pricing could affect 
+            your future profitability.
+          </p>
+          <p className="results-text">Click on a Calculator above to begin!</p>
+        </div>
         <QuestionCheckboxes />
       </div>
     </center>
