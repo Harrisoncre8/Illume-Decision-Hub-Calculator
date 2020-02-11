dd867015adf9a229e273ef2d36f65526fa83595c
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 21:29:13 -0600

    created container for list items, left-justified text, added margins to top and bottom

--- src/components/QuestionCheckboxes/QuestionCheckboxes.js
+++ src/components/QuestionCheckboxes/QuestionCheckboxes.js
@@ -45,40 +45,42 @@ const QuestionCheckboxes = () => {
 
   return(
     <center style={{backgroundColor: 'white'}}>
-      <h1>Which costs that are relevant to you?</h1>
+      <h1>Which costs are relevant to you?</h1>
       <form onSubmit={(e)=>submit(e)}>
-        {Array.isArray(questions)? 
-          questions.map(question=>{
-            if(!question.sub_questions && !question.split && question.header !== 'Product/Service Price' && question.header !== 'Revenue' && question.header !== 'Number of Sales'){
-              return(
-                <div key={question.id}>
-                  <div>
-                    <input type='checkbox' checked={checked[question.id]} onChange={()=>toggleChecked(question.id)} />
-                    <span className="questions-checkbox-text">{question.header}</span>
+        <div className="questions-checkbox-container">
+          {Array.isArray(questions)? 
+            questions.map(question=>{
+              if(!question.sub_questions && !question.split && question.header !== 'Product/Service Price' && question.header !== 'Revenue' && question.header !== 'Number of Sales'){
+                return(
+                  <div key={question.id}>
+                    <div>
+                      <input type='checkbox' checked={checked[question.id]} onChange={()=>toggleChecked(question.id)} />
+                      <span className="questions-checkbox-text">{question.header}</span>
+                    </div>
+                    {questions.map(subQuestion=>{
+                      if(subQuestion.sub_questions===question.id){
+                        return(
+                          <div key={subQuestion.id}>
+                            <input type='checkbox' checked={checked[subQuestion.id]} onChange={()=>toggleChecked(subQuestion.id)} />
+                            <span className="questions-checkbox-text">{subQuestion.header}</span>
+                          </div>
+                        );
+                      } 
+                      else {
+                        return(null);
+                      };
+                    })}
                   </div>
-                  {questions.map(subQuestion=>{
-                    if(subQuestion.sub_questions===question.id){
-                      return(
-                        <div key={subQuestion.id}>
-                          <input type='checkbox' checked={checked[subQuestion.id]} onChange={()=>toggleChecked(subQuestion.id)} />
-                          <span className="questions-checkbox-text">{subQuestion.header}</span>
-                        </div>
-                      );
-                    } 
-                    else {
-                      return(null);
-                    };
-                  })}
-                </div>
-              )} 
-              else {
-                return(null);
-              };
-          })
-          :
-          null
-        }
-        <button className="normal-btn" type="submit">Save</button>
+                )} 
+                else {
+                  return(null);
+                };
+            })
+            :
+            null
+          }
+        </div>
+        <button className="normal-btn questions-checkbox-btn" type="submit">Save</button>
       </form>
     </center>
   );
