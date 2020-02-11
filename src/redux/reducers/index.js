192d6de1cbfa877446d2d9ccaaae92387cea128c
Courtney Olesee <court.olson1@gmail.com>
2020-02-11 15:49:30 -0600

    alphabetized reducers in root

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -17,20 +17,20 @@ const createReducer = (string, type=[]) => {
 }
 
 const rootReducer = combineReducers({
-  errors, // KEEP FILE // contains registrationMessage and login Message
-  question: createReducer('SET_QUESTION'), // stores questions from the database
   adminIndustry: createReducer('SET_ADMIN_INDUSTRY'), // stores admin industry information
   adminQuestion: createReducer('SET_ADMIN_QUESTION'), // stores calculator questions for admin to edit
-  adminUserInfo: createReducer('SET_ADMIN_USER_INFO'), // stores user information for admin to edit
   adminSubquestion: createReducer('SET_ADMIN_SUB_QUESTION'), // store calculator sub-questions for admin to edit
+  adminUserInfo: createReducer('SET_ADMIN_USER_INFO'), // stores user information for admin to edit
+  errors, // KEEP FILE // contains registrationMessage and login Message
   industry: createReducer('SET_INDUSTRY'), // stores industry information
-  split: createReducer('SET_SPLIT'), // stores split from the database
-  user: createReducer('SET_USER', {}), // stores user info - basic table (not contact info table)
+  input, // KEEP FILE // stores values from inputs from stepper - has a second clear in reducer listening for ALL
   passwordStatus: createReducer('MATCH_PASSWORD'), // contains password change status
   previousQuestion, // KEEP FILE // stores id of previous question for stepper - has a second clear in reducer listening for ALL
-  input, // KEEP FILE // stores values from inputs from stepper - has a second clear in reducer listening for ALL
-  userInfo: createReducer('SET_USER_INFO'), // stores necessary information for user edit page - this is contact info
+  question: createReducer('SET_QUESTION'), // stores questions from the database
+  split: createReducer('SET_SPLIT'), // stores split from the database
+  user: createReducer('SET_USER', {}), // stores user info - basic table (not contact info table)
   userCheckboxes: createReducer('SET_USER_CHECKBOXES'), // holds the users checkboxes
+  userInfo: createReducer('SET_USER_INFO'), // stores necessary information for user edit page - this is contact info
 });
 
 export default rootReducer;
