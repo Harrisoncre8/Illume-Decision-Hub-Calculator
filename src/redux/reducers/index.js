654e46915626d861767310b536a1c9fb91b7d145
Courtney Olesee <court.olson1@gmail.com>
2020-02-11 10:08:17 -0600

    reducers without switch simplified

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,27 +1,25 @@
 import { combineReducers } from 'redux';
 import errors from './errorsReducer';
 import admin from './adminReducer';
-import industry from './industryReducer';
-import question from './questionReducer';
-import split from './splitReducer';
 import user from './userReducer';
 import previousQuestion from './previousQuestionReducer';
 import input from './inputReducer';
-import userInfo from './userInfoReducer';
-import passwordStatus from './passwordStatusReducer';
 import userCheckboxes from './userCheckboxesReducer';
+
+const createReducer = (string, type=[]) => (state=type, action) => action.type === string ? action.payload : state;
+
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and login Message
-  question, // stores questions from the database
+  question: createReducer('SET_QUESTION'), // stores questions from the database
   admin, // stores admin information
-  industry, // stores industry information
-  split, // stores split from the database
+  industry: createReducer('SET_INDUSTRY'), // stores industry information
+  split: createReducer('SET_SPLIT'), // stores split from the database
   user,
-  passwordStatus, // contains password change status
+  passwordStatus: createReducer('MATCH_PASSWORD'), // contains password change status
   previousQuestion, // stores id of previous question for stepper
   input, // stores values from inputs from stepper
-  userInfo, // stores necessary information for user edit page
+  userInfo: createReducer('SET_USER_INFO'), // stores necessary information for user edit page
   userCheckboxes, // holds the users checkboxes
 });
 
