a4a5d6c4d9a1cf87e06a087569df7cb196d8620c
AwrenNuit <a.rennaker@yahoo.com>
2020-02-10 21:20:02 -0600

    set button and text classes, minor formatting updates

--- src/components/QuestionCheckboxes/QuestionCheckboxes.js
+++ src/components/QuestionCheckboxes/QuestionCheckboxes.js
@@ -7,74 +7,80 @@ const QuestionCheckboxes = () => {
   const userCheckboxes = useSelector(state=>state.userCheckboxes);
 
   useEffect(()=>{
-    dispatch({type: 'GET_ALL_QUESTIONS'})
-  }, [dispatch])
+    dispatch({type: 'GET_ALL_QUESTIONS'});
+  }, [dispatch]);
 
-  const [checked, setChecked] = useState({})
+  const [checked, setChecked] = useState({});
 
   useEffect(()=>{
     if(userCheckboxes[0] && userCheckboxes[0].user_id){
-      const holder = {}
+      const holder = {};
       userCheckboxes.forEach(el=>{
         holder[el.question_id] = true;
       });
-      setChecked(holder)
+      setChecked(holder);
     } else if (Array.isArray(questions)) {
       const state = questions.reduce((acum,arr)=>{
         acum[arr.id] = true;
         return acum;
       },{})
-      setChecked(state)
+      setChecked(state);
     }
-  },[questions,userCheckboxes])
+  },[questions,userCheckboxes]);
 
   function toggleChecked(id){
     let holder = {...checked};
     holder[id] = !checked[id];
     setChecked(holder);
   }
+
   function submit(e){
     e.preventDefault();
-    let popup = window.confirm('Save your preferences?')
+    let popup = window.confirm('Save your preferences?');
     if(popup){
-      dispatch({type: 'SET_CHECKBOXES', payload: checked})
+      dispatch({type: 'SET_CHECKBOXES', payload: checked});
     }
-
   }
 
   return(
     <center style={{backgroundColor: 'white'}}>
-      <h1>Please select costs that are relevent to you</h1>
+      <h1>Please select costs that are relevant to you</h1>
       <form onSubmit={(e)=>submit(e)}>
-        {Array.isArray(questions)? questions.map(question=>{
-          if(!question.sub_questions && !question.split && question.header !== 'Product/Service Price' && question.header !== 'Revenue' && question.header !== 'Number of Sales'){
-            return(
-              <div key={question.id}>
-                <div>
-                  <input type='checkbox' checked={checked[question.id]} onChange={(e)=>{toggleChecked(question.id)}} />
-                  {question.header}
+        {Array.isArray(questions)? 
+          questions.map(question=>{
+            if(!question.sub_questions && !question.split && question.header !== 'Product/Service Price' && question.header !== 'Revenue' && question.header !== 'Number of Sales'){
+              return(
+                <div key={question.id}>
+                  <div>
+                    <input type='checkbox' checked={checked[question.id]} onChange={()=>toggleChecked(question.id)} />
+                    <span className="questions-checkbox-text">{question.header}</span>
+                  </div>
+                  {questions.map(subQuestion=>{
+                    if(subQuestion.sub_questions===question.id){
+                      return(
+                        <div key={subQuestion.id}>
+                          <input type='checkbox' checked={checked[subQuestion.id]} onChange={()=>toggleChecked(subQuestion.id)} />
+                          <span className="questions-checkbox-text">{subQuestion.header}</span>
+                        </div>
+                      );
+                    } 
+                    else {
+                      return(null);
+                    };
+                  })}
                 </div>
-                {questions.map(subQuestion=>{
-                  if(subQuestion.sub_questions===question.id){
-                    return(
-                      <div key={subQuestion.id}>
-                        <input type='checkbox' checked={checked[subQuestion.id]} onChange={(e)=>{toggleChecked(subQuestion.id)}}/>
-                        {subQuestion.header}
-                      </div>
-                    );
-                  } else {
-                    return(null);
-                  };
-                })}
-              </div>
-            )} else {
-              return(null);
-            };
-        }):null}
-        <button>Save</button>
+              )} 
+              else {
+                return(null);
+              };
+          })
+          :
+          null
+        }
+        <button className="normal-btn" type="submit">Save</button>
       </form>
     </center>
-  )
+  );
 };
 
 export default QuestionCheckboxes;
