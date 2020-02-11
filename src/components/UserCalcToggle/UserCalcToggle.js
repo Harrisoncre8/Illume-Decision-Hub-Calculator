2dc90d1b14ea7a188e1925582a4f86cc0df00b32
Harrison Nguyen <nguy2173@umn.edu>
2020-02-11 16:22:10 -0600

    Staged handle calculator click for dispatch to saga

--- src/components/UserCalcToggle/UserCalcToggle.js
+++ src/components/UserCalcToggle/UserCalcToggle.js
@@ -4,17 +4,24 @@ import { useDispatch, useSelector } from 'react-redux';
 const UserCalcToggle = () => {
 
   let dispatch = useDispatch();
+  let userID = useSelector(state => state.user.id);
+
+  // handle button click to toggle nav 
+  const handleCalcClick = calcID => {
+    let calcInfo = {userID, calcID}
+    dispatch({ type: 'TOGGLE_CALC', payload: calcInfo});
+  }
 
   return(
     <div>
       <h2>Toggle Your Calculators</h2>
       <p>* Select a calculator to turn on and off</p>
       <p>* Disabled calculators will be colored out</p>
-      <button className={`circle-btn`} onClick={() => dispatch({type: `TOGGLE_NAV`, payload: 1})}>
-        Break even Calculator</button>
-      <button className={`circle-btn`} onClick={() => dispatch({type: `TOGGLE_NAV`, payload: 2})}>
+      <button className={`circle-btn`} onClick={() => handleCalcClick(1)}>
         Profit Lever Calculator</button>
-      <button className={`circle-btn`} onClick={() => dispatch({type: `TOGGLE_NAV`, payload: 3})}>
+      <button className={`circle-btn`} onClick={() => handleCalcClick(2)}>
+        Break even Calculator</button>
+      <button className={`circle-btn`} onClick={() => handleCalcClick(3)}>
         Price Setting Calculator</button>
     </div>
   )
