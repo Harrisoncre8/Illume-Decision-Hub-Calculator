fa5ee73cd37583f176600c4cb693d490a2c02b5f
AwrenNuit <a.rennaker@yahoo.com>
2020-02-11 16:25:24 -0600

    created 404 eclipse moon

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,12 +6,16 @@ export default function NotFound() {
 
     return(
     <center>
-      <Link to="/">
-        <div className="nf-logo-div">
-          <img className="nf-logo" src="illume-logo180.png" alt="illume pricing logo" />
-        </div>
-        <h1 className="main-heading nf-heading">Illume Decision Hub Home</h1>
-      </Link>
+      <div className="nf-container">
+        <h1>Oops! This page does not exist!</h1>
+        <div className="nf-moon"><span className="nf-moon-text">404</span></div>
+        <Link to="/">
+          <div className="nf-logo-div">
+            <img className="nf-logo" src="illume-logo180.png" alt="illume pricing logo" />
+          </div>
+          <h1 className="main-heading nf-heading">Illume Decision Hub Home</h1>
+        </Link>
+      </div>
     </center>
   );
 }
