4b094055182d8996df1e204b93e5728d99a9b715
Harrison Nguyen <nguy2173@umn.edu>
2020-02-12 15:47:25 -0600

    Add button class change feature on calculator toggle

--- src/components/UserCalcToggle/UserCalcToggle.js
+++ src/components/UserCalcToggle/UserCalcToggle.js
@@ -8,27 +8,10 @@ const UserCalcToggle = () => {
   let userID = useSelector(state => state.user.id);
   let calcData = useSelector(state => state.calcStatus);
 
-  // setting state to change button class name
-  const [breakEven, setBreakEven] = useState('circle-btn');
-  const [lever, setLever] = useState('circle-btn');
-  const [price, setPrice] = useState('circle-btn');
-  const [calculator, setCalculator] = useState('');
-
   useEffect(() => {
     if(userID){
       dispatch({type: `GET_CALC_INFO`, payload: userID});
     }
-    // if(
-    //   calcData[calcData.findIndex(element => element.calculator_id === calculator)] &&
-    //   calcData[calcData.findIndex(element => element.calculator_id === calculator)].calculator_id === 1
-    // ){
-    //   setLever(`circle-btn-active`)
-    //   console.log('ASDFASFAFASDFAFASDFAFAS', lever);
-    // } else if(calcData[calcData.findIndex(element => element.calculator_id === calculator)] === 2){
-    //   setPrice(`circle-btn-active`)
-    // } else if(calcData[calcData.findIndex(element => element.calculator_id === calculator)] === 3){
-    //   setBreakEven(`circle-btn-active`)
-    // }
   }, [userID, dispatch]);
 
   // handle button click to toggle nav 
@@ -38,21 +21,31 @@ const UserCalcToggle = () => {
       dispatch({type: `DELETE_CALC`, payload: calcInfo});
     } else {
       dispatch({type: `TOGGLE_CALC`, payload: calcInfo});
-      setCalculator(calcID);
+    }
+  }
+
+  // handle button class change based on calculator toggle
+  const getButtonClass = (calcID) => {
+    let calcExist = calcData[calcData.findIndex(element => element.calculator_id === calcID)];
+    if(calcExist && calcExist.calculator_id === calcID
+    ){
+      return `circle-btn circle-btn-active`;
+    } else {
+      return `circle-btn`;
     }
   }
 
   return(
     <div>
       {JSON.stringify(calcData)}
-      <h2>Toggle Your Calculators</h2>
+]      <h2>Toggle Your Calculators</h2>
       <p>* Select a calculator to turn on and off</p>
       <p>* Disabled calculators will be colored out</p>
-      <button className={`nav-btn ${lever}`} onClick={() => handleCalcClick(1)}>
+      <button className={getButtonClass(1)} onClick={() => handleCalcClick(1)}>
         Profit Lever Calculator</button>
-      <button className={`nav-btn ${breakEven}`} onClick={() => handleCalcClick(2)}>
+      <button className={getButtonClass(2)} onClick={() => handleCalcClick(2)}>
         Break even Calculator</button>
-      <button className={`nav-btn ${price}`} onClick={() => handleCalcClick(3)}>
+      <button className={getButtonClass(3)} onClick={() => handleCalcClick(3)}>
         Price Setting Calculator</button>
     </div>
   )
