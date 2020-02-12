6af753224142aebae270a5c8e773946598a0aea5
AwrenNuit <a.rennaker@yahoo.com>
2020-02-12 15:43:58 -0600

    reformatted dispatch and states

--- src/components/CheckIndustry/CheckIndustry.js
+++ src/components/CheckIndustry/CheckIndustry.js
@@ -8,19 +8,17 @@ export default function CheckIndustry() {
   const dispatch = useCallback(useDispatch());
   const industryData = useSelector(state => state.industry);
   const userData = useSelector(state => state.userInfo);
-  let userID = useSelector(state => state.user.id);
-  const [id, setId] = useState('');
+  const userID = useSelector(state => state.user.id);
   const [industry, setIndustry] = useState('');
   const [modal, setModal] = useState(false);
 
   // Run on component mount
   useEffect(()=>{
     if(userID){
-      setId(userID);
       dispatch({type: `GET_USER_INFO`, payload: userID});
       dispatch({type: `GET_INDUSTRY`});
     }
-  }, [userID]);
+  }, [dispatch, userID]);
 
   // Open Modal if industry was disabled
   useEffect(()=>{
@@ -49,7 +47,6 @@ export default function CheckIndustry() {
         effect="fadeInUp"
       >
         <div className="modal-container">
-          {JSON.stringify(userData)}
           <h1 className="main-heading">Update Industry</h1>
           <div className="update-industry-p-container">
             <p className="align-left">Your industry has been removed.</p>
@@ -65,7 +62,7 @@ export default function CheckIndustry() {
               <option 
                 className="dropdown-option" 
                 key={industry.id} 
-                value={industry.industry}
+                value={industry.id}
               >
                 {industry.industry}
               </option>
