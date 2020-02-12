828465b4cbb6e35ff90cf701ccc3c3898ef8456b
AwrenNuit <a.rennaker@yahoo.com>
2020-02-12 10:16:50 -0600

    added terms-of-service agreement checkbox

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -15,23 +15,18 @@ class Register extends Component{
     email: '',
     password: '',
     showPassword: 'password',
-    agreement: false
+    agreement: false,
   }
 
   // Toggle agreement checkbox
-  agree = e => this.state.agreement ? this.setState({agreement: false}) : this.setState({agreement: true});
+  agree = e => this.state.agreement ? this.setState({agreement:false}) : this.setState({agreement:true});
 
   // Close modal popup
-  closeModal = () => this.setState({ visible: false });
+  closeModal = () => this.setState({visible:false});
 
   // Adds class if input has a value, removes the class if input has no value
   checkForValue = e => e.target.value ? e.target.classList.add('text-field-active') : e.target.classList.remove('text-field-active');
 
-  // Set local state agreement to false, close modal popup
-  disagree = () => {
-    this.setState({agreement: false});
-    this.closeModal();
-  }
   // Sets local state to current input value, adds or removes class based on input's value
   handleChange = (e, propName) => {
     this.setState({
@@ -47,7 +42,7 @@ class Register extends Component{
   }
 
   // Open modal popup
-  openModal = () => this.setState({ visible: true });
+  openModal = () => this.setState({visible:true});
 
   // Push history to new user page
   pushHistoryToUser = () => {
@@ -57,23 +52,19 @@ class Register extends Component{
   // Log new user information into database, push history to new user page
   registerUser = e => {
     e.preventDefault();
-    if (this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password){
-      this.openModal();
-      if(this.state.agreement && !this.state.visible){
-        console.log('in there------------------------------------------');
-        this.props.dispatch({
-          type: 'REGISTER',
-          payload: {
-            name: this.state.name,
-            company: this.state.company,
-            phone: this.state.phone,
-            industry: this.state.industry,
-            email: this.state.email,
-            password: this.state.password,
-          },
-        });           
-        this.pushHistoryToUser();
-      }
+    if (this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password && this.state.agreement){
+      this.props.dispatch({
+        type: 'REGISTER',
+        payload: {
+          name: this.state.name,
+          company: this.state.company,
+          phone: this.state.phone,
+          industry: this.state.industry,
+          email: this.state.email,
+          password: this.state.password,
+        },
+      });
+      this.pushHistoryToUser();
     } else {
       this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
     }
@@ -165,9 +156,17 @@ class Register extends Component{
             <div className="text-field-mask register-mask-password"></div>
             <span>
               <input type="checkbox" onClick={this.togglePasswordView} />
-                <label> Show Password</label>
+              <label> Show Password</label>
             </span>
           </div>
+
+          <div>
+            <input type="checkbox" onClick={this.agree} />
+            <label> I agree to the <span className="tos" onClick={this.openModal}>Terms of Service</span></label>
+          </div>
+
+          {JSON.stringify(this.state.agreement)}
+
           <button className="normal-btn register-register-btn" onClick={this.registerUser}>Confirm</button>
           <hr className="register-hr" />
 
@@ -178,24 +177,16 @@ class Register extends Component{
               width="440"
               height="500"
               effect="fadeInUp"
+              onClickAway={this.closeModal}
             >
               <div className="modal-container">
                 <h1 className="main-heading modal-heading">Usage Agreement</h1>
                 <div>
                   <p>{this.props.disclaimer}</p>
                 </div>
-                <div>
-                  <label className="checkbox-container">I AGREE
-                    <input type='checkbox' checked={this.state.agreement} onChange={this.agree} />
-                    <span className="checkbox-check"></span>
-                  </label>
-                </div>
                 <div className="modal-btn-container">
                   <button className="normal-btn profile-modal-btns" onClick={this.closeModal}>
-                    Confirm
-                  </button>
-                  <button className="normal-btn" onClick={this.disagree}>
-                    Cancel
+                    Okay
                   </button>
                 </div>
               </div>
