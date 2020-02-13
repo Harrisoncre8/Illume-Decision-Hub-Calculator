07ee74bfdc3b1cdee17a7f3a2707029f05f8ccd8
Phillip Berg <phillipb1991@msn.com>
2020-02-13 08:47:17 -0600

    DB industries are now displayed instead of dummy data

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Register.css';
 import Modal from 'react-awesome-modal';
 
-class Register extends Component{
+class Register extends Component {
 
   //Store local state
   state = {
@@ -20,14 +20,15 @@ class Register extends Component{
 
   // Run on component mount
   componentDidMount() {
-    this.props.dispatch({type: `GET_DISCLAIMER`});
+    this.props.dispatch({ type: `GET_DISCLAIMER` });
+    this.props.dispatch({ type: 'GET_INDUSTRY' })
   }
 
   // Toggle agreement checkbox
-  agree = e => this.state.agreement ? this.setState({agreement:false}) : this.setState({agreement:true});
+  agree = e => this.state.agreement ? this.setState({ agreement: false }) : this.setState({ agreement: true });
 
   // Close modal popup
-  closeModal = () => this.setState({visible:false});
+  closeModal = () => this.setState({ visible: false });
 
   // Adds class if input has a value, removes the class if input has no value
   checkForValue = e => e.target.value ? e.target.classList.add('text-field-active') : e.target.classList.remove('text-field-active');
@@ -40,14 +41,14 @@ class Register extends Component{
     });
     this.checkForValue(e);
   }
-  
+
   // Return to login page
   handleCancel = () => {
     this.props.history.push('/');
   }
 
   // Open modal popup
-  openModal = () => this.setState({visible:true});
+  openModal = () => this.setState({ visible: true });
 
   // Push history to new user page
   pushHistoryToUser = () => {
@@ -57,7 +58,7 @@ class Register extends Component{
   // Log new user information into database, push history to new user page
   registerUser = e => {
     e.preventDefault();
-    if (this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password && this.state.agreement){
+    if (this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password && this.state.agreement) {
       this.props.dispatch({
         type: 'REGISTER',
         payload: {
@@ -70,30 +71,30 @@ class Register extends Component{
         },
       });
       this.pushHistoryToUser();
-    } 
-    else if(this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password){
-      this.props.dispatch({type: `REGISTRATION_TOS_NOT_CHECKED`});
+    }
+    else if (this.state.name && this.state.company && this.state.phone && this.state.industry && this.state.email && this.state.password) {
+      this.props.dispatch({ type: `REGISTRATION_TOS_NOT_CHECKED` });
     }
     else {
-      this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
+      this.props.dispatch({ type: 'REGISTRATION_INPUT_ERROR' });
     }
   }
 
   // Show or hide password
-  togglePasswordView = () => this.state.showPassword === 'password' ? this.setState({showPassword: 'text'}) : this.setState({showPassword: 'password'});
+  togglePasswordView = () => this.state.showPassword === 'password' ? this.setState({ showPassword: 'text' }) : this.setState({ showPassword: 'password' });
 
-  render(){
-    return(
+  render() {
+    return (
       <center>
         <div className="register-container">
-        {this.props.errors.registrationMessage && (
-          <h2
-            className="alert"
-            role="alert"
-          >
-            {this.props.errors.registrationMessage}
-          </h2>
-        )}
+          {this.props.errors.registrationMessage && (
+            <h2
+              className="alert"
+              role="alert"
+            >
+              {this.props.errors.registrationMessage}
+            </h2>
+          )}
 
           <div>
             <img className="register-logo" src="illume-logo180.png" alt="illume logo" />
@@ -101,72 +102,74 @@ class Register extends Component{
           </div>
 
           <div className="text-field-container">
-            <input 
-              className="text-field" 
-              type="text" 
+            <input
+              className="text-field"
+              type="text"
               name="name"
               value={this.state.name}
-              onChange={(e)=>this.handleChange(e, 'name')}
+              onChange={(e) => this.handleChange(e, 'name')}
             />
             <label className="text-field-label">name</label>
             <div className="text-field-mask register-mask-name"></div>
           </div>
 
           <div className="text-field-container">
-            <input 
-              className="text-field" 
-                type="text" 
-                name="company"
-                value={this.state.company}
-                onChange={(e)=>this.handleChange(e, 'company')}
+            <input
+              className="text-field"
+              type="text"
+              name="company"
+              value={this.state.company}
+              onChange={(e) => this.handleChange(e, 'company')}
             />
             <label className="text-field-label">company</label>
             <div className="text-field-mask register-mask-company"></div>
           </div>
 
           <div className="text-field-container">
-            <input 
-              className="text-field" 
-              type="text" 
+            <input
+              className="text-field"
+              type="text"
               name="phone"
               value={this.state.phone}
-              onChange={(e)=>this.handleChange(e, 'phone')}
+              onChange={(e) => this.handleChange(e, 'phone')}
             />
             <label className="text-field-label">phone #</label>
             <div className="text-field-mask register-mask-phone"></div>
           </div>
 
           <div className="text-field-container">
-            <select 
+            <select
               className="dropdown register-dropdown"
               name="industry"
               value={this.state.industry}
-              onChange={(e)=>this.handleChange(e, 'industry')}
+              onChange={(e) => this.handleChange(e, 'industry')}
             >
               <option className="dropdown-option" value='' disabled>Select Industry</option>
-              <option className="dropdown-option" value={1}>Attorney</option>
-              <option className="dropdown-option" value={2}>Cleaning</option>
-              <option className="dropdown-option" value={3}>Massage</option>
+              {this.props.industry.map((industryRow=>{
+                return(
+                  <option className="dropdown-option" value ={industryRow.id}>{industryRow.industry}</option>
+                )
+              }))}
             </select>
           </div>
 
           <div className="text-field-container">
-            <input 
-              className="text-field" 
-              type="text" 
+            <input
+              className="text-field"
+              type="text"
               name="email"
               value={this.state.email}
-              onChange={(e)=>this.handleChange(e, 'email')}
+              onChange={(e) => this.handleChange(e, 'email')}
             />
             <label className="text-field-label">email</label>
             <div className="text-field-mask register-mask-email"></div>
           </div>
 
           <div className="text-field-container">
-            <input 
-              className="text-field" 
-              type={this.state.showPassword} 
-              onChange={(e)=>this.handleChange(e, 'password')}
+            <input
+              className="text-field"
+              type={this.state.showPassword}
+              onChange={(e) => this.handleChange(e, 'password')}
             />
             <label className="text-field-label">password</label>
             <div className="text-field-mask register-mask-password"></div>
@@ -186,33 +189,34 @@ class Register extends Component{
           <button className="register-cancel-btn" onClick={this.handleCancel}>cancel</button>
 
           <Modal
-              visible={this.state.visible}
-              width="440"
-              height="330"
-              effect="fadeInUp"
-              onClickAway={this.closeModal}
-            >
-              <div className="modal-container">
-                <h1 className="main-heading">Usage Agreement</h1>
-                <div>
-                  <p className="align-left">{this.props.disclaimer.disclaimer}</p>
-                </div>
-                <div className="modal-btn-container">
-                  <button className="normal-btn" onClick={this.closeModal}>
-                    Okay
-                  </button>
-                </div>
+            visible={this.state.visible}
+            width="440"
+            height="330"
+            effect="fadeInUp"
+            onClickAway={this.closeModal}
+          >
+            <div className="modal-container">
+              <h1 className="main-heading">Usage Agreement</h1>
+              <div>
+                <p className="align-left">{this.props.disclaimer.disclaimer}</p>
               </div>
-              </Modal>
+              <div className="modal-btn-container">
+                <button className="normal-btn" onClick={this.closeModal}>
+                  Okay
+                  </button>
+              </div>
+            </div>
+          </Modal>
         </div>
       </center>
     );
   }
 }
 
-const putReduxStateOnProps = reduxState=>({
+const putReduxStateOnProps = reduxState => ({
   disclaimer: reduxState.disclaimer,
   errors: reduxState.errors,
+  industry: reduxState.industry,
 });
 
 export default connect(putReduxStateOnProps)(Register);
