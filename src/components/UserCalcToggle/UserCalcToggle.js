b38eb77fd366ea28d9c6e561c74303b6feaca300
AwrenNuit <a.rennaker@yahoo.com>
2020-02-12 20:01:33 -0600

    created stylesheet, spaced out toggle buttons, added grey background to disabled buttons

--- src/components/UserCalcToggle/UserCalcToggle.js
+++ src/components/UserCalcToggle/UserCalcToggle.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import './UserCalcToggle.css';
 
 const UserCalcToggle = () => {
 
   // getting data from reducer
-  let dispatch = useDispatch();
-  let userID = useSelector(state => state.user.id);
-  let calcData = useSelector(state => state.calcStatus);
+  const dispatch = useDispatch();
+  const userID = useSelector(state => state.user.id);
+  const calcData = useSelector(state => state.calcStatus);
 
   useEffect(() => {
     if(userID){
@@ -19,18 +20,19 @@ const UserCalcToggle = () => {
     let calcInfo = {userID, calcID};
     if(calcData[calcData.findIndex(element => element.calculator_id === calcID)]){
       dispatch({type: `DELETE_CALC`, payload: calcInfo});
-    } else {
+    } 
+    else {
       dispatch({type: `TOGGLE_CALC`, payload: calcInfo});
     }
   }
 
   // handle button class change based on calculator toggle
-  const getButtonClass = (calcID) => {
+  const getButtonClass = calcID => {
     let calcExist = calcData[calcData.findIndex(element => element.calculator_id === calcID)];
-    if(calcExist && calcExist.calculator_id === calcID
-    ){
-      return `circle-btn circle-btn-active`;
-    } else {
+    if(calcExist && calcExist.calculator_id === calcID){
+      return `circle-btn circle-btn-disabled`;
+    } 
+    else {
       return `circle-btn`;
     }
   }
@@ -39,15 +41,26 @@ const UserCalcToggle = () => {
     <div>
       <h2>Toggle Your Calculators</h2>
       <p>* Select a calculator to turn on and off</p>
-      <p>* Disabled calculators will be colored out</p>
-      <button className={getButtonClass(1)} onClick={() => handleCalcClick(1)}>
-        Profit Lever Calculator</button>
-      <button className={getButtonClass(2)} onClick={() => handleCalcClick(2)}>
-        Break even Calculator</button>
-      <button className={getButtonClass(3)} onClick={() => handleCalcClick(3)}>
-        Price Setting Calculator</button>
+      <p>* Disabled calculators will appear grey</p>
+      <div className="toggle-btn-main-container">
+        <span className="toggle-btn-container">
+          <button className={getButtonClass(1)} onClick={() => handleCalcClick(1)}>
+            Profit Lever Calculator
+          </button>
+        </span>
+        <span className="toggle-btn-container">
+        <button className={getButtonClass(2)} onClick={() => handleCalcClick(2)}>
+          Break even Calculator
+        </button>
+        </span>
+        <span className="toggle-btn-container">
+        <button className={getButtonClass(3)} onClick={() => handleCalcClick(3)}>
+          Price Setting Calculator
+        </button>
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default UserCalcToggle;
