1e1e814e2441a0e2ee66ed4e29a9bfd766a87d38
AwrenNuit <a.rennaker@yahoo.com>
2020-02-14 11:41:54 -0600

    created slide 5

--- src/components/Slide4/Slide4.js
+++ src/components/Slide4/Slide4.js
@@ -8,7 +8,7 @@ export default function Slide4() {
     <div className="list-slide-main-container">
       <h1 className="list-slide-heading">Targets</h1>
       <ul>
-        <li>Woman-owned businesses</li>
+        <li>Woman-owned small businesses</li>
         <li>Conversational language</li>
         <li>Increasing overall finances</li>
       </ul>
