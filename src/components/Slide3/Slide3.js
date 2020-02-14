85cbad991068ef811a02b1e645b0deabccb442b7
AwrenNuit <a.rennaker@yahoo.com>
2020-02-14 11:29:00 -0600

    created stylesheet, set up closing slide

--- src/components/Slide3/Slide3.js
+++ src/components/Slide3/Slide3.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import '../Slide1/Slide1.css';
-import { useHistory } from 'react-router-dom';
+import './Slide3.css';
 
 export default function Slide1() {
 
@@ -9,11 +8,18 @@ export default function Slide1() {
       <div className="slide-main-container">
 
         <div className="slide-heading-container">
-        <img className="slide-logo" src="illume-logo180.png" alt="Illume Logo" />
-          <span className="slide-heading">TEAM ILLUME</span>
+        <img className="slide3-logo" src="illume-logo180.png" alt="Illume Logo" />
+          <span className="slide3-heading">team illume</span>
         </div>
 
-        <div className="slide-img-name-container">
+        <div className="slide3-img-name-container-clients">
+          <img className="slide3-img img-1" src="https://illume-pricing.com/wp-content/uploads/2019/07/Susan-outside-2.jpeg" alt="Susan Heinlein" />
+            <div className="slide3-name n1">Susan Heinlein</div>
+          <img className="slide3-img img-2" src="https://illume-pricing.com/wp-content/uploads/2019/07/Jenny-Niemela-1024x1024.jpg" alt="Jenny Niemela" />
+            <div className="slide3-name n2">Jenny Niemela</div>
+        </div>
+
+        <div className="slide3-img-name-container-team">
           <img className="slide-img img-1" src="https://avatars1.githubusercontent.com/u/52988745?s=460&v=4" alt="Harrison Nguyen" />
             <div className="slide-name n1">Harrison Nguyen</div>
           <img className="slide-img img-2" src="https://avatars2.githubusercontent.com/u/54560441?s=460&v=4" alt="Awren Nuit" />
