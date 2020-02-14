bfb491bdaa9b25fa5aafc1f94af229f03e4f6e5e
AwrenNuit <a.rennaker@yahoo.com>
2020-02-14 11:38:30 -0600

    created Targets slide

---
+++ src/components/Slide4/Slide4.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const history = useHistory();
+
+export default function Slide4() {
+  return(
+    <div className="list-slide-main-container">
+      <h1 className="list-slide-heading">Targets</h1>
+      <ul>
+        <li>Woman-owned businesses</li>
+        <li>Conversational language</li>
+        <li>Increasing overall finances</li>
+      </ul>
+    </div>
+  );
+}
