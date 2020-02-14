c74a4dfe875f1ccee7d26734a7ba05b378d87207
AwrenNuit <a.rennaker@yahoo.com>
2020-02-14 17:36:07 -0600

    converted component to use hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,128 +1,108 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, {useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './Login.css';
 
-class Login extends Component{
+export default function Login() {
 
-  // Store local state
-  state = {
-    email: '',
-    password: '',
-    showPassword: 'password'
-  }
+  const dispatch = useCallback(useDispatch());
+  const history = useHistory();
+  const user = useSelector(state => state.user);
+  const error = useSelector(state => state.errors);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState('password');
 
-  // Use previous props to check user type for route
-  componentDidUpdate(prevProps){
-    if(this.props.user !== prevProps.user){
-      this.pushHistoryToUser();
+  // Push history based on user type
+  useEffect(()=>{
+    if(user && user.admin){
+      history.push('/admin');
     }
-  }
+    else if(user && user.id){
+      history.push('/user');
+    }
+    else {
+      history.push('/');
+    }
+  }, [history, user]);
 
-  // Adds class if input has a value, removes the class if input has no value
-  checkForValue = e => e.target.value ? e.target.classList.add('text-field-active') : e.target.classList.remove('text-field-active');
-
-  // Set local state to current input value, adds or removes class based on input's value
-  handleChange = (e, propName) => {
-    this.setState({
-      ...this.state,
-      [propName]: e.target.value
-    });
-    this.checkForValue(e);
-  }
+  // Add class if input has a value, removes the class if input has no value
+  const checkForValue = e => e.target.value ? e.target.classList.add('text-field-active') : e.target.classList.remove('text-field-active');
 
   // Push history to registration page
-  handleRegister = () => this.props.history.push('/register');
+  const handleRegister = () => history.push('/register');
 
   // Handle user log-in and push history to main user page, otherwise return an error message
-  login = e => {
+  const login = e => {
     e.preventDefault();
-    if (this.state.email && this.state.password) {
-      this.props.dispatch({
-        type: 'LOGIN',
-        payload: {
-          username: this.state.email,
-          password: this.state.password,
-        },
-      });
-    } 
-    else {
-      this.props.dispatch({ type: 'LOGIN_INPUT_ERROR' });
-    }
-  }
-
-  // Push user to admin or user based on admin boolean
-  pushHistoryToUser = () => {
-    if(this.props.user && this.props.user.admin){
-      this.props.history.push('/admin');
-    }
-    else if(this.props.user && this.props.user.id){
-      this.props.history.push('/user');
+    if(email && password){
+      dispatch({type: `LOGIN`, payload: {username: email, password: password}});
     }
     else {
-      this.props.history.push('/');
+      dispatch({type: `LOGIN_INPUT_ERROR`});
     }
   }
 
   // Show or hide password
-  togglePasswordView = () => this.state.showPassword === 'password' ? this.setState({showPassword: 'text'}) : this.setState({showPassword: 'password'});
+  const togglePasswordView = () => showPassword === 'password' ? setShowPassword('text') : setShowPassword('password');
 
-  render(){
-    return(
-      <center>
-        <div className="login-container">
-        {this.props.errors.loginMessage && (
-              <h2
-                className="alert"
-                role="alert"
-              >
-                {this.props.errors.loginMessage}
-              </h2>
-          )}
-          <div>
-            <img className="login-logo" src="illume-logo180.png" alt="illume logo" />
-            <span className="login-brand-name">illume decision hub</span>
-          </div>
-          <form onSubmit={this.login}>
-
-          <div className="text-field-container login-text-field-container">
-            <input 
-              className="text-field" 
-              type="text" 
-              onChange={(e)=>this.handleChange(e, 'email')}
-            />
-            <label className="text-field-label">email</label>
-            <div className="text-field-mask login-email-mask"></div>
-          </div>
-
-          <div className="text-field-container login-text-field-container">
-            <input 
-              className="text-field" 
-              type={this.state.showPassword} 
-              onChange={(e)=>this.handleChange(e, 'password')}
-            />
-
-            <label className="text-field-label">password</label>
-            <div className="text-field-mask login-password-mask"></div>
-              <span>
-                <input type="checkbox" onChange={this.togglePasswordView} />
-                  <label> Show Password</label>
-              </span>
-            </div>
-
-          <button className="normal-btn login-login-btn" type="submit" onClick={this.login}>Log In</button>
-          </form>
-          <hr className="login-hr" />
-
-          <button className="login-register-btn" onClick={this.handleRegister}>register</button>
+  return(
+    <center>
+      <div className="login-container">
+      {error.loginMessage && (
+            <h2
+              className="alert"
+              role="alert"
+            >
+              {error.loginMessage}
+            </h2>
+        )}
+        <div>
+          <img className="login-logo" src="illume-logo180.png" alt="illume logo" />
+          <span className="login-brand-name">illume decision hub</span>
         </div>
-      </center>
-    );
-  }
+
+        <form onSubmit={login}>
+
+        <div className="text-field-container login-text-field-container">
+          <input 
+            className="text-field" 
+            type="text" 
+            onChange={(e)=>{
+                        setEmail(e.target.value);
+                        checkForValue(e);
+                        }
+                      }
+          />
+          <label className="text-field-label">email</label>
+          <div className="text-field-mask login-email-mask"></div>
+        </div>
+
+        <div className="text-field-container login-text-field-container">
+          <input 
+            className="text-field" 
+            type={showPassword} 
+            onChange={(e)=>{
+                        setPassword(e.target.value);
+                        checkForValue(e);
+                        }
+                      }
+          />
+
+          <label className="text-field-label">password</label>
+          <div className="text-field-mask login-password-mask"></div>
+            <span>
+              <input type="checkbox" onChange={togglePasswordView} />
+                <label> Show Password</label>
+            </span>
+          </div>
+
+        <button className="normal-btn login-login-btn" type="submit">Log In</button>
+        </form>
+        <hr className="login-hr" />
+
+        <button className="login-register-btn" onClick={handleRegister}>register</button>
+      </div>
+    </center>
+  );
 }
-
-const mapStateToProps = state => ({
-    errors: state.errors,
-    user: state.user,
-});
-
-export default connect(mapStateToProps)(Login);
