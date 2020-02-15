4b2c4f959740d42fbe34a179609ae1de44e9f847
AwrenNuit <a.rennaker@yahoo.com>
2020-02-15 14:07:51 -0600

    created component

---
+++ src/components/Slide6/Slide6.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Slide6() {
+  
+  const history = useHistory();
+
+  return(
+    <center>
+      <div className="list-slide-main-container" onClick={()=>history.push(`/slide3`)}>
+        <img className="slide-logo" src="illume-logo180.png" alt="Illume Logo" />
+            <span className="slide-heading">illume decision hub</span>
+        <ul className="list-ul">
+          <li className="list-li">Dynamic information gathering</li>
+          <li className="list-li">Conversational language</li>
+          <li className="list-li">Prompted consideration</li>
+          <li className="list-li">Profit opportunities</li>
+          <li className="list-li">Predictive calculations</li>
+        </ul>
+      </div>
+    </center>
+  );
+}
