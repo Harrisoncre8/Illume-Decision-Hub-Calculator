88ba5ac190d0a5d01115eff6e55ddd0407bb97cd
AwrenNuit <a.rennaker@yahoo.com>
2020-02-15 14:03:45 -0600

    formatted component

--- src/components/Slide5/Slide5.js
+++ src/components/Slide5/Slide5.js
@@ -6,17 +6,18 @@ export default function Slide5() {
   const history = useHistory();
 
   return(
-    <div className="list-slide-main-container" onClick={()=>history.push(`/`)}>
-      <img className="slide-logo" src="illume-logo180.png" alt="Illume Logo" />
-          <span className="slide-heading">illume decision hub</span>
-      <ul>
-        <li>Dynamic information gathering</li>
-        <li>Conversational language</li>
-        <li>Prompted consideration</li>
-        <li>Profit Opportunities</li>
-        <li>Predictive calculations</li>
-
-      </ul>
-    </div>
+    <center>
+      <div className="list-slide-main-container" onClick={()=>history.push(`/`)}>
+        <img className="slide-logo" src="illume-logo180.png" alt="Illume Logo" />
+            <span className="slide-heading">illume decision hub</span>
+        <ul className="list-ul">
+          <li className="list-li">Dynamic information gathering</li>
+          <li className="list-li">Conversational language</li>
+          <li className="list-li">Prompted consideration</li>
+          <li className="list-li">Profit Opportunities</li>
+          <li className="list-li">Predictive calculations</li>
+        </ul>
+      </div>
+    </center>
   );
 }
