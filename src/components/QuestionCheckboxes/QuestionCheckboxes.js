7a8c57ba76788d24345b3ba2271df50eccadff48
Phillip Berg <phillipb1991@msn.com>
2020-02-16 10:05:38 -0600

    overkill on what checks are checked

--- src/components/QuestionCheckboxes/QuestionCheckboxes.js
+++ src/components/QuestionCheckboxes/QuestionCheckboxes.js
@@ -44,9 +44,18 @@ const QuestionCheckboxes = () => {
         }
         return acum;
       },{});
+      state[1] = true;
+      state[2] = true;
+      state[3] = true;
+      state[4] = true;
+      state[5] = true;
+      state[6] = true;
+      state[7] = true;
+      state[9] = true;
       state[16] = true;
       state[18] = true;
       state[19] = true;
+      state[22] = true;
       setChecked(state);
     }
     if(Array.isArray(questions)){
