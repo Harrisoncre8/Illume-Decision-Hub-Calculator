af809e5bcce633e6c97f28b7aabc9a4fdc9c7efa
AwrenNuit <a.rennaker@yahoo.com>
2020-02-16 14:38:31 -0600

    added image files for phillip, jenny, and susan

--- src/components/Slide1/Slide1.js
+++ src/components/Slide1/Slide1.js
@@ -20,7 +20,7 @@ export default function Slide1() {
             <div className="slide-name n1">Harrison Nguyen</div>
           <img className="slide-img img-2" src="https://avatars2.githubusercontent.com/u/54560441?s=460&v=4" alt="Awren Nuit" />
             <div className="slide-name n2">Awren Nuit</div>
-          <img className="slide-img img-3" src="https://avatars1.githubusercontent.com/u/44420714?s=460&v=4" alt="Phillip Berg" />
+          <img className="slide-img img-3" src="phillip.jpg" alt="Phillip Berg" />
             <div className="slide-name n3">Phillip Berg</div>
           <img className="slide-img img-4" src="https://avatars0.githubusercontent.com/u/55601904?s=460&v=4" alt="Courtney Olesee" />
             <div className="slide-name n4">Courtney Olesee</div>
--- src/components/Slide2/Slide2.js
+++ src/components/Slide2/Slide2.js
@@ -16,9 +16,9 @@ export default function Slide2() {
         </div>
 
         <div className="slide2-img-name-container">
-          <img className="slide2-img img-1" src="https://illume-pricing.com/wp-content/uploads/2019/07/Susan-outside-2.jpeg" alt="Susan Heinlein" />
+          <img className="slide2-img img-1" src="susan.jpeg" alt="Susan Heinlein" />
             <div className="slide2-name n1">Susan Heinlein</div>
-          <img className="slide2-img img-2" src="https://illume-pricing.com/wp-content/uploads/2019/07/Jenny-Niemela-1024x1024.jpg" alt="Jenny Niemela" />
+          <img className="slide2-img img-2" src="jenny.jpg" alt="Jenny Niemela" />
             <div className="slide2-name n2">Jenny Niemela</div>
         </div>
 
