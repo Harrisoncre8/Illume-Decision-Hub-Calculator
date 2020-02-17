120618337266b2494f51ff94de5f513138e4e7c5
AwrenNuit <a.rennaker@yahoo.com>
2020-02-17 12:39:17 -0600

    minor formatting updates

--- src/components/Slide6/Slide6.js
+++ src/components/Slide6/Slide6.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function Slide6() {
 
   const history = useHistory();
+
+  useEffect(()=>{
+    for(let i=0; i<1; i++){
+      let audio = new Audio('thx.mp3');
+      audio.play();
+    }
+  }, []);
 
   return(
     <center>
